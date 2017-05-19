f4fbff03b8b488f9f7022068f6e67b6ff9404f80
Abel Barrientos <abelbs27101@gmail.com>
2017-05-19 22:34:10 +0000

    Fixing checkstyle issues

--- src/main/java/org/fundacionjala/coding/abel/BankOCR.java
+++ src/main/java/org/fundacionjala/coding/abel/BankOCR.java
@@ -1,19 +1,19 @@
-package org.fundacionjala.coding;
+package org.fundacionjala.coding.abel;
 
 /**
  * Created by Administrator on 5/19/2017.
  */
 public class BankOCR {
-    private static final String ZERO  = " _ | ||_|";
-    private static final String ONE   = "     |  |";
-    private static final String TWO   = " _  _||_ ";
+    private static final String ZERO = " _ | ||_|";
+    private static final String ONE = "     |  |";
+    private static final String TWO = " _  _||_ ";
     private static final String THREE = " _  _| _|";
-    private static final String FOUR  = "   |_|  |";
-    private static final String FIVE  = " _ |_  _|";
-    private static final String SIX   = " _ |_ |_|";
+    private static final String FOUR = "   |_|  |";
+    private static final String FIVE = " _ |_  _|";
+    private static final String SIX = " _ |_ |_|";
     private static final String SEVEN = " _   |  |";
     private static final String EIGHT = " _ |_||_|";
-    private static final String NINE  = " _ |_| _|";
+    private static final String NINE = " _ |_| _|";
 
     private static final String[] ALL_NUMBERS = {ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE};
 
@@ -24,19 +24,17 @@ public class BankOCR {
 
         String result = "";
 
-        for (String string: positionNumbers)
-        {
+        for (String string : positionNumbers) {
             result += checkString(string);
         }
 
         return result;
     }
 
-    private static String[] getPositionNumbers(char[] charArray){
+    private static String[] getPositionNumbers(char[] charArray) {
         String[] positionNumbers = new String[9];
 
-        for (int i = 0; i < 9; i++)
-        {
+        for (int i = 0; i < 9; i++) {
             positionNumbers[i] = extractNumbers(charArray, i);
         }
 
@@ -45,8 +43,7 @@ public class BankOCR {
 
     private static String checkString(String string) {
         String result = "";
-        for (int i = 0; i < 9; i++)
-        {
+        for (int i = 0; i < 9; i++) {
             result = getNumberValue(string);
         }
         return result;
@@ -54,9 +51,8 @@ public class BankOCR {
 
     private static String getNumberValue(String string) {
         String result = "";
-        for (int j = 0; j < ALL_NUMBERS.length; j++)
-        {
-            if(string.equals(ALL_NUMBERS[j]))
+        for (int j = 0; j < ALL_NUMBERS.length; j++) {
+            if (string.equals(ALL_NUMBERS[j]))
                 result = String.valueOf(j);
         }
         return result;
@@ -64,7 +60,7 @@ public class BankOCR {
 
 
     private static String extractNumbers(char[] charArray, int position) {
-        return  charArray[0 + position * 3] + "" + charArray[1 + position * 3] + "" + charArray[2 + position * 3] + "" +
+        return charArray[0 + position * 3] + "" + charArray[1 + position * 3] + "" + charArray[2 + position * 3] + "" +
                 charArray[27 + position * 3] + "" + charArray[28 + position * 3] + "" + charArray[29 + position * 3] + "" +
                 charArray[54 + position * 3] + "" + charArray[55 + position * 3] + "" + charArray[56 + position * 3];
     }
--- src/test/java/org/fundacionjala/coding/abel/BankOCRTest.java
+++ src/test/java/org/fundacionjala/coding/abel/BankOCRTest.java
@@ -1,5 +1,6 @@
-package org.fundacionjala.coding;
+package org.fundacionjala.coding.abel;
 
+import org.fundacionjala.coding.abel.BankOCR;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -10,9 +11,8 @@ import static org.junit.Assert.assertEquals;
 public class BankOCRTest {
 
     @Test
-    public void testBankOCRStoryOne()
-    {
-        String entry =  "    _  _     _  _  _  _  _ " +
+    public void testBankOCRStoryOne() {
+        String entry = "    _  _     _  _  _  _  _ " +
                 "  | _| _||_||_ |_   ||_||_|" +
                 "  ||_  _|  | _||_|  ||_| _|";
 
@@ -21,116 +21,106 @@ public class BankOCRTest {
     }
 
     @Test
-    public void testBankOCRStoryOneJustZeros()
-    {
-        String entry =  " _  _  _  _  _  _  _  _  _ " +
+    public void testBankOCRStoryOneJustZeros() {
+        String entry = " _  _  _  _  _  _  _  _  _ " +
                 "| || || || || || || || || |" +
                 "|_||_||_||_||_||_||_||_||_|";
 
         String expectedResult = "000000000";
-        assertEquals(expectedResult,  BankOCR.convertEntryToNumber(entry));
+        assertEquals(expectedResult, BankOCR.convertEntryToNumber(entry));
     }
 
     @Test
-    public void testBankOCRStoryOneJustOnes()
-    {
-        String entry =  "                           " +
+    public void testBankOCRStoryOneJustOnes() {
+        String entry = "                           " +
                 "  |  |  |  |  |  |  |  |  |" +
                 "  |  |  |  |  |  |  |  |  |";
 
         String expectedResult = "111111111";
-        assertEquals(expectedResult,  BankOCR.convertEntryToNumber(entry));
+        assertEquals(expectedResult, BankOCR.convertEntryToNumber(entry));
     }
 
     @Test
-    public void testBankOCRStoryOneJustTwos()
-    {
-        String entry =  " _  _  _  _  _  _  _  _  _ " +
+    public void testBankOCRStoryOneJustTwos() {
+        String entry = " _  _  _  _  _  _  _  _  _ " +
                 " _| _| _| _| _| _| _| _| _|" +
                 "|_ |_ |_ |_ |_ |_ |_ |_ |_ ";
 
         String expectedResult = "222222222";
-        assertEquals(expectedResult,  BankOCR.convertEntryToNumber(entry));
+        assertEquals(expectedResult, BankOCR.convertEntryToNumber(entry));
     }
 
     @Test
-    public void testBankOCRStoryOneJustThrees()
-    {
-        String entry =  " _  _  _  _  _  _  _  _  _ " +
+    public void testBankOCRStoryOneJustThrees() {
+        String entry = " _  _  _  _  _  _  _  _  _ " +
                 " _| _| _| _| _| _| _| _| _|" +
                 " _| _| _| _| _| _| _| _| _| ";
 
         String expectedResult = "333333333";
-        assertEquals(expectedResult,  BankOCR.convertEntryToNumber(entry));
+        assertEquals(expectedResult, BankOCR.convertEntryToNumber(entry));
     }
 
 
     @Test
-    public void testBankOCRStoryOneJustFours()
-    {
-        String entry =  "                           " +
+    public void testBankOCRStoryOneJustFours() {
+        String entry = "                           " +
                 "|_||_||_||_||_||_||_||_||_|" +
                 "  |  |  |  |  |  |  |  |  |";
 
         String expectedResult = "444444444";
-        assertEquals(expectedResult,  BankOCR.convertEntryToNumber(entry));
+        assertEquals(expectedResult, BankOCR.convertEntryToNumber(entry));
     }
 
 
     @Test
-    public void testBankOCRStoryOneJustFives()
-    {
-        String entry =  " _  _  _  _  _  _  _  _  _ " +
+    public void testBankOCRStoryOneJustFives() {
+        String entry = " _  _  _  _  _  _  _  _  _ " +
                 "|_ |_ |_ |_ |_ |_ |_ |_ |_ " +
                 " _| _| _| _| _| _| _| _| _|";
 
         String expectedResult = "555555555";
-        assertEquals(expectedResult,  BankOCR.convertEntryToNumber(entry));
+        assertEquals(expectedResult, BankOCR.convertEntryToNumber(entry));
     }
 
     @Test
-    public void testBankOCRStoryOneJustSixs()
-    {
-        String entry =  " _  _  _  _  _  _  _  _  _ " +
+    public void testBankOCRStoryOneJustSixs() {
+        String entry = " _  _  _  _  _  _  _  _  _ " +
                 "|_ |_ |_ |_ |_ |_ |_ |_ |_ " +
                 "|_||_||_||_||_||_||_||_||_|";
 
         String expectedResult = "666666666";
-        assertEquals(expectedResult,  BankOCR.convertEntryToNumber(entry));
+        assertEquals(expectedResult, BankOCR.convertEntryToNumber(entry));
     }
 
 
     @Test
-    public void testBankOCRStoryOneJustSevens()
-    {
-        String entry =  " _  _  _  _  _  _  _  _  _ " +
+    public void testBankOCRStoryOneJustSevens() {
+        String entry = " _  _  _  _  _  _  _  _  _ " +
                 "  |  |  |  |  |  |  |  |  |" +
                 "  |  |  |  |  |  |  |  |  |";
 
         String expectedResult = "777777777";
-        assertEquals(expectedResult,  BankOCR.convertEntryToNumber(entry));
+        assertEquals(expectedResult, BankOCR.convertEntryToNumber(entry));
     }
 
     @Test
-    public void testBankOCRStoryOneJustEights()
-    {
-        String entry =  " _  _  _  _  _  _  _  _  _ " +
+    public void testBankOCRStoryOneJustEights() {
+        String entry = " _  _  _  _  _  _  _  _  _ " +
                 "|_||_||_||_||_||_||_||_||_|" +
                 "|_||_||_||_||_||_||_||_||_|";
 
         String expectedResult = "888888888";
-        assertEquals(expectedResult,  BankOCR.convertEntryToNumber(entry));
+        assertEquals(expectedResult, BankOCR.convertEntryToNumber(entry));
     }
 
 
     @Test
-    public void testBankOCRStoryOneJustNines()
-    {
-        String entry =  " _  _  _  _  _  _  _  _  _ " +
+    public void testBankOCRStoryOneJustNines() {
+        String entry = " _  _  _  _  _  _  _  _  _ " +
                 "|_||_||_||_||_||_||_||_||_|" +
                 " _| _| _| _| _| _| _| _| _|";
 
         String expectedResult = "999999999";
-        assertEquals(expectedResult,  BankOCR.convertEntryToNumber(entry));
+        assertEquals(expectedResult, BankOCR.convertEntryToNumber(entry));
     }
 }
