22adae37936db0aadcf210a68d6f36a19a466e56
Abel Barrientos <abelbs27101@gmail.com>
2017-05-22 11:15:29 -0400

    Added Number class, change method getNumberValue to use stream filter.

--- src/main/java/org/fundacionjala/coding/abel/BankOCR.java
+++ src/main/java/org/fundacionjala/coding/abel/BankOCR.java
@@ -1,67 +1,129 @@
 package org.fundacionjala.coding.abel;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
- * Created by Administrator on 5/19/2017.
+ * Created by AbelBarrientos on 16/5/2017.
  */
-public class BankOCR {
-    private static final String ZERO = " _ | ||_|";
-    private static final String ONE = "     |  |";
-    private static final String TWO = " _  _||_ ";
+public final class BankOCR {
+
+    private static final String ZERO  = " _ | ||_|";
+    private static final String ONE   = "     |  |";
+    private static final String TWO   = " _  _||_ ";
     private static final String THREE = " _  _| _|";
-    private static final String FOUR = "   |_|  |";
-    private static final String FIVE = " _ |_  _|";
-    private static final String SIX = " _ |_ |_|";
+    private static final String FOUR  = "   |_|  |";
+    private static final String FIVE  = " _ |_  _|";
+    private static final String SIX   = " _ |_ |_|";
     private static final String SEVEN = " _   |  |";
     private static final String EIGHT = " _ |_||_|";
-    private static final String NINE = " _ |_| _|";
+    private static final String NINE  = " _ |_| _|";
 
-    private static final String[] ALL_NUMBERS = {ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE};
+    private static final List<Number> ALL_NUMBERS = Arrays.asList(
+            new Number(ZERO, 0),
+            new Number(ONE, 1),
+            new Number(TWO, 2),
+            new Number(THREE, 3),
+            new Number(FOUR, 4),
+            new Number(FIVE, 5),
+            new Number(SIX, 6),
+            new Number(SEVEN, 7),
+            new Number(EIGHT, 8),
+            new Number(NINE, 9));
+    public static final int TOTAL_CHARS = 9;
+    public static final int CHAR_LENGTH = 3;
+    public static final int FIRST_LINE_AT_ZERO = 0;
+    public static final int FIRST_LINE_AT_ONE = 1;
+    public static final int FIRST_LINE_AT_TWO = 2;
+    public static final int SECOND_LINE_AT_ZERO = 27;
+    public static final int SECOND_LINE_AT_ONE = 28;
+    public static final int SECOND_LINE_AT_TWO = 29;
+    public static final int THIRD_LINE_AT_ZERO = 54;
+    public static final int THIRD_LINE_AT_ONE = 55;
+    public static final int THIRD_LINE_AT_TWO = 56;
 
+    /**
+     *
+     */
+    private BankOCR() {
+    }
+
+    /**
+     * Read the entry.
+     * @param entry is the parameter
+     * @return String
+     */
     public static String convertEntryToNumber(String entry) {
         char[] charArray = entry.toCharArray();
 
         String[] positionNumbers = getPositionNumbers(charArray);
 
-        String result = "";
+        StringBuffer stringBuffer = new StringBuffer();
 
-        for (String string : positionNumbers) {
-            result += checkString(string);
+        for (String string: positionNumbers) {
+            stringBuffer.append(checkString(string));
         }
 
-        return result;
+        return stringBuffer.toString();
     }
 
+    /**
+     *
+     * @param charArray parameter
+     * @return String[]
+     */
     private static String[] getPositionNumbers(char[] charArray) {
-        String[] positionNumbers = new String[9];
+        String[] positionNumbers = new String[TOTAL_CHARS];
 
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < TOTAL_CHARS; i++) {
             positionNumbers[i] = extractNumbers(charArray, i);
         }
 
         return positionNumbers;
     }
 
+    /**
+     *
+     * @param string parameter
+     * @return String
+     */
     private static String checkString(String string) {
         String result = "";
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < TOTAL_CHARS; i++) {
             result = getNumberValue(string);
         }
         return result;
     }
 
+    /**
+     *
+     * @param string parameter
+     * @return String
+     */
     private static String getNumberValue(String string) {
-        String result = "";
-        for (int j = 0; j < ALL_NUMBERS.length; j++) {
-            if (string.equals(ALL_NUMBERS[j]))
-                result = String.valueOf(j);
-        }
-        return result;
+        Number result = ALL_NUMBERS.stream()
+                .filter(number -> string.equals(number.getStringRepresentation()))
+                .findAny()
+                .orElse(null);
+
+        return String.valueOf(result.getIntValue());
     }
 
-
+    /**
+     *
+     * @param charArray parameter
+     * @param position parameter
+     * @return String
+     */
     private static String extractNumbers(char[] charArray, int position) {
-        return charArray[0 + position * 3] + "" + charArray[1 + position * 3] + "" + charArray[2 + position * 3] + "" +
-                charArray[27 + position * 3] + "" + charArray[28 + position * 3] + "" + charArray[29 + position * 3] + "" +
-                charArray[54 + position * 3] + "" + charArray[55 + position * 3] + "" + charArray[56 + position * 3];
+        return  charArray[FIRST_LINE_AT_ZERO + position * CHAR_LENGTH] + ""
+                + charArray[FIRST_LINE_AT_ONE + position * CHAR_LENGTH] + ""
+                + charArray[FIRST_LINE_AT_TWO + position * CHAR_LENGTH] + ""
+                + charArray[SECOND_LINE_AT_ZERO + position * CHAR_LENGTH] + ""
+                + charArray[SECOND_LINE_AT_ONE + position * CHAR_LENGTH] + ""
+                + charArray[SECOND_LINE_AT_TWO + position * CHAR_LENGTH] + ""
+                + charArray[THIRD_LINE_AT_ZERO + position * CHAR_LENGTH] + ""
+                + charArray[THIRD_LINE_AT_ONE + position * CHAR_LENGTH] + ""
+                + charArray[THIRD_LINE_AT_TWO + position * CHAR_LENGTH];
     }
 }
