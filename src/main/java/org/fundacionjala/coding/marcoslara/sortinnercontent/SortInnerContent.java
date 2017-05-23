4a5976321963e41077fc28d1ffa38f3992ce6960
marcos-x86 <marcos.x86@outlook.com>
2017-05-23 18:26:59 -0400

    2nd Commit

--- src/main/java/org/fundacionjala/coding/marcoslara/sortinnercontent/SortInnerContent.java
+++ src/main/java/org/fundacionjala/coding/marcoslara/sortinnercontent/SortInnerContent.java
@@ -1,7 +1,75 @@
 package org.fundacionjala.coding.marcoslara.sortinnercontent;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
- * Created by Administrator on 5/23/2017.
+ * Created by Marcos Lara on 5/23/2017.
  */
 public class SortInnerContent {
+    private String words;
+    private static final int MINIMUM_STRING_LENGTH = 3;
+
+    /**
+     * Constructor of the class.
+     *
+     * @param words The string to sort inner content.
+     */
+    public SortInnerContent(String words) {
+        this.words = words;
+    }
+
+    /**
+     * The main method that does the transformation of the string.
+     *
+     * @return The sort inner content as a String
+     */
+    public String getSortInnerContent() {
+        StringBuffer resultWords = new StringBuffer();
+        String[] splitWords = words.split(" ");
+        for (int i = 0; i < splitWords.length; i++) {
+            if (splitWords[i].length() <= MINIMUM_STRING_LENGTH) {
+                resultWords.append(splitWords[i]);
+            } else {
+                resultWords.append(splitWords[i].charAt(0));
+                resultWords.append(getDescendingOrder(splitWords[i].substring(1, splitWords[i].length() - 1)));
+                resultWords.append(splitWords[i].charAt(splitWords[i].length() - 1));
+            }
+            if (i + 1 < splitWords.length) {
+                resultWords.append(" ");
+            }
+        }
+        return resultWords.toString();
+    }
+
+    /**
+     * This method performs the descending order of a string.
+     *
+     * @param substring The substring to be ordered in descending order
+     * @return The substring ordered in descending order
+     */
+    private String getDescendingOrder(String substring) {
+        char[] charArray = substring.toCharArray();
+        List<Character> listCharacters = new ArrayList<>();
+        for (char c : charArray) {
+            listCharacters.add(c);
+        }
+        Collections.sort(listCharacters, Collections.reverseOrder());
+        return getStringFromList(listCharacters);
+    }
+
+    /**
+     * This method converts a list of characters to a String.
+     *
+     * @param listCharacters List containing the characters to be concatenated in a single string.
+     * @return The concatenated string.
+     */
+    private String getStringFromList(List<Character> listCharacters) {
+        StringBuilder stringConstructed = new StringBuilder(listCharacters.size());
+        for (Character character : listCharacters) {
+            stringConstructed.append(character);
+        }
+        return stringConstructed.toString();
+    }
 }
--- src/test/java/org/fundacionjala/coding/marcoslara/sortinnercontent/SortInnerContentTest.java
+++ src/test/java/org/fundacionjala/coding/marcoslara/sortinnercontent/SortInnerContentTest.java
@@ -1,7 +1,90 @@
 package org.fundacionjala.coding.marcoslara.sortinnercontent;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
- * Created by Administrator on 5/23/2017.
+ * Created by Marcos Lara on 5/23/2017.
  */
 public class SortInnerContentTest {
+    /**
+     * Test that tests the Sort Inner Content for an empty string.
+     */
+    @Test
+    public void getSortInnerContentOfAEmptyStringIsAEmptyString() {
+        SortInnerContent sortInnerContent = new SortInnerContent("");
+
+        assertEquals("", sortInnerContent.getSortInnerContent());
+    }
+
+    /**
+     * Test that tests the Sort Inner Content for a string with a letter.
+     */
+    @Test
+    public void getSortInnerContentOfAStringWithOneLetterIsTheSameLetter() {
+        SortInnerContent sortInnerContent = new SortInnerContent("a");
+
+        assertEquals("a", sortInnerContent.getSortInnerContent());
+    }
+
+    /**
+     * Test that tests the Sort Inner Content for a string with three letters.
+     */
+    @Test
+    public void getSortInnerContentOfAStringWithThreeLettersIsTheSameString() {
+        SortInnerContent sortInnerContent = new SortInnerContent("abc");
+
+        assertEquals("abc", sortInnerContent.getSortInnerContent());
+    }
+
+    /**
+     * Test that tests the Sort Inner Content for a string with six letters.
+     */
+    @Test
+    public void getSortInnerContentOfAStringWithSixLetters() {
+        SortInnerContent sortInnerContent = new SortInnerContent("abcdef");
+
+        assertEquals("aedcbf", sortInnerContent.getSortInnerContent());
+    }
+
+    /**
+     * Test that tests the Sort Inner Content for a string with two words.
+     */
+    @Test
+    public void getSortInnerContentOfAStringWithTwoWords() {
+        SortInnerContent sortInnerContent = new SortInnerContent("time gaming");
+
+        assertEquals("tmie gnmiag", sortInnerContent.getSortInnerContent());
+    }
+
+    /**
+     * Test that tests the Sort Inner Content for a string (example 1).
+     */
+    @Test
+    public void getSortInnerContentOfAExampleStringNumberOne() {
+        SortInnerContent sortInnerContent = new SortInnerContent("sort the inner content in descending order");
+
+        assertEquals("srot the inner ctonnet in dsnnieedcg oredr", sortInnerContent.getSortInnerContent());
+    }
+
+    /**
+     * Test that tests the Sort Inner Content for a string (example 2).
+     */
+    @Test
+    public void getSortInnerContentOfAExampleStringNumberTwo() {
+        SortInnerContent sortInnerContent = new SortInnerContent("wait for me");
+
+        assertEquals("wiat for me", sortInnerContent.getSortInnerContent());
+    }
+
+    /**
+     * Test that tests the Sort Inner Content for a string (example 3).
+     */
+    @Test
+    public void getSortInnerContentOfAExampleStringNumberThree() {
+        SortInnerContent sortInnerContent = new SortInnerContent("this kata is easy");
+
+        assertEquals("tihs ktaa is esay", sortInnerContent.getSortInnerContent());
+    }
 }
