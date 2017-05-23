1c683a19d3ef85df2a275b78d82a23f6ebac22ed
marcos-x86 <marcos.x86@outlook.com>
2017-05-23 11:56:21 -0400

    Coding - Kata #1 - Spin Words Initial commit.

---
+++ src/main/java/org/fundacionjala/coding/marcoslara/spinwords/SpinWords.java
@@ -0,0 +1,7 @@
+package org.fundacionjala.coding;
+
+/**
+ * Created by Administrator on 5/19/2017.
+ */
+public class SpinWords {
+}
---
+++ src/test/java/org/fundacionjala/coding/marcoslara/spinwords/SpinWordsTest.java
@@ -0,0 +1,7 @@
+package org.fundacionjala.coding.marcoslara.spinwords;
+
+/**
+ * Created by Administrator on 5/19/2017.
+ */
+public class SpinWordsTest {
+}
