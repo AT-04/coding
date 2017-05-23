aef9113c93ed924a9c3016c6f4c2cb9c911cb91c
Marcandea <marcandea2@gmail.com>
2017-05-23 16:01:11 -0400

    Ultimate Spin Word

--- src/main/java/org/fundacionjala/coding/SpinWord.java
+++ src/main/java/org/fundacionjala/coding/SpinWord.java
@@ -1,7 +1,63 @@
 package org.fundacionjala.coding;
 
+import java.util.StringJoiner;
+
 /**
  * Created by MarcandeaCabrera on 5/19/2017.
  */
+
 public class SpinWord {
+    private String cadena;
+    private String[] auxiliarWord;
+    private StringJoiner stringJoiner;
+    public static final int LENGTH_REVERSE = 5;
+
+    /**
+     * The following method is the constructor.
+     *
+     * @param cadena send for make the operation.
+     */
+    public SpinWord(String cadena) {
+        this.cadena = cadena;
+        stringJoiner = new StringJoiner(" ");
+    }
+
+    /**
+     * Reverse the words.
+     *
+     * @param string return reverse word.
+     * @return string
+     */
+    public String reverse(String string) {
+        StringBuffer stringBuffer = new StringBuffer(string);
+        stringBuffer.reverse();
+        return stringBuffer.toString();
+    }
+
+    /**
+     * Call process.
+     *
+     * @return string
+     */
+    public String reverseProcess() {
+        auxiliarWord = cadena.split(" ");
+        return reverseWord();
+    }
+
+    /**
+     * Reverse Word major to five character.
+     *
+     * @return string
+     */
+    public String reverseWord() {
+        for (String word : auxiliarWord) {
+            if (word.length() > LENGTH_REVERSE) {
+                word = reverse(word);
+                stringJoiner.add(word);
+            } else {
+                stringJoiner.add(word);
+            }
+        }
+        return stringJoiner.toString();
+    }
 }
--- src/test/java/org/fundacionjala/coding/SpinWordTest.java
+++ src/test/java/org/fundacionjala/coding/SpinWordTest.java
@@ -1,7 +1,31 @@
 package org.fundacionjala.coding;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by MarcandeaCabrera on 5/19/2017.
  */
 public class SpinWordTest {
+
+    /**
+     *Verify words less than or equal to five characters.
+     */
+    @Test
+    public void wordLessOrEqualsThanFiveCharacters() {
+        SpinWord cad = new SpinWord("Hola mundo");
+        assertEquals("Hola mundo", cad.reverseProcess());
+    }
+
+    /**
+     *Verify words smaller or longer or equals than five characters.
+     */
+    @Test
+    public void wordLessEqualsOrLongerThanFiveCharacters() {
+        SpinWord cad = new SpinWord("Hola mundos marcandea");
+        assertEquals("Hola sodnum aednacram", cad.reverseProcess());
+    }
+
+
 }
