9398939e6c81d062a535384fb5e35610d5932adf
John Doe <yveryury@gmail.com>
2017-05-25 17:33:07 -0400

    Finish the Sort Inner Content Kata 2

--- src/main/java/org/fundacionjala/coding/yury/InnerContent.java
+++ src/main/java/org/fundacionjala/coding/yury/InnerContent.java
@@ -1,7 +1,39 @@
 package main.java.org.fundacionjala.coding.yury;
 
+import java.util.Arrays;
+
+
 /**
  * Created by YuryOrtuno on 5/25/2017.
  */
 public class InnerContent {
+
+    /**
+     * @param string content the sentence for sort inner content
+     * @return the sentece whit Sort Inner Content
+     */
+    public String innerWord(String string) {
+        String[] splitWords = string.split(" ");
+        StringBuffer stringBuffer = new StringBuffer();
+        int i = 0;
+        for (String word : splitWords) {
+            char[] character = word.substring(1, word.length() - 1).toCharArray();
+            Arrays.sort(character);
+            if (i < splitWords.length - 1) {
+                stringBuffer.append(" ");
+            }
+            stringBuffer.append(word.charAt(word.length() - 1));
+            stringBuffer.append(character);
+            stringBuffer.append(word.charAt(0));
+            splitWords[i] = stringBuffer.reverse().toString();
+            i++;
+            stringBuffer.delete(0, stringBuffer.length());
+        }
+
+        for (String sentence : splitWords) {
+            stringBuffer.append(sentence);
+        }
+        return stringBuffer.toString();
+
+    }
 }
--- src/test/java/org/fundacionjala/coding/yury/InnerContentTest.java
+++ src/test/java/org/fundacionjala/coding/yury/InnerContentTest.java
@@ -1,7 +1,42 @@
 package test.java.org.fundacionjala.coding.yury;
 
+import main.java.org.fundacionjala.coding.yury.InnerContent;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by YuryOrtuno on 5/25/2017.
  */
 public class InnerContentTest {
+    /**
+     *
+     */
+    @Test
+    public void innerOneWord() {
+        InnerContent innerContent = new InnerContent();
+        assertEquals("srot", innerContent.innerWord("sort"));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void innerTwoWord() {
+        InnerContent innerContent = new InnerContent();
+        assertEquals("inner ctonnet", innerContent.innerWord("inner content"));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void innerSentence() {
+        InnerContent innerContent = new InnerContent();
+        assertEquals("srot the inner ctonnet in dsnnieedcg oredr",
+                innerContent.innerWord("sort the inner content in descending order"));
+    }
+
+
 }
+
