6d9f7beb28a5061b097c92601033459f01bba30b
Marcandea <marcandea2@gmail.com>
2017-05-26 18:55:36 -0400

    Finish first Test from SortContent

--- src/main/java/org/fundacionjala/coding/Marcan/SortContent.java
+++ src/main/java/org/fundacionjala/coding/Marcan/SortContent.java
@@ -10,20 +10,22 @@ public class SortContent {
     /**
      * The following method is for reverse one word.
      *
-     * @param string
+     * @param string The test string is sent.
      * @return stringBuilder
      */
     public String sortContent(String string) {
         String[] words = string.split(" ");
-        StringBuilder stringBuilder = new StringBuilder();
+        StringJoiner stringJoiner = new StringJoiner(" ");
         for (String word : words) {
             char[] wordInChar = word.substring(1, word.length() - 1).toCharArray();
             Arrays.sort(wordInChar);
-            stringBuilder.append(word.substring(word.length() - 1, word.length()));
-            stringBuilder.append(wordInChar);
-            stringBuilder.append(word.substring(0, 1));
-
+            StringBuilder str = new StringBuilder();
+            str.append(word.substring(word.length() - 1, word.length()));
+            str.append(wordInChar);
+            str.append(word.substring(0, 1));
+            String reversed = str.reverse().toString();
+            stringJoiner.add(reversed);
         }
-        return stringBuilder.reverse().toString();
+        return stringJoiner.toString();
     }
 }
--- src/test/java/org/fundacionjala/coding/Marcan/SortContentTest.java
+++ src/test/java/org/fundacionjala/coding/Marcan/SortContentTest.java
@@ -9,12 +9,20 @@ import static org.junit.Assert.assertEquals;
  */
 public class SortContentTest {
     /**
-     * This test is for verify word inverse.
+     * This test is for verify to characters inverse inside of the word.
      */
     @Test
-    public void firstTest(){
-        SortContent sortContent=new SortContent();
-        assertEquals("Mrcaan",sortContent.sortContent("Marcan"));
+    public void verifyOneWord() {
+        SortContent sortContent = new SortContent();
+        assertEquals("Conidg", sortContent.sortContent("Coding"));
     }
 
+    /**
+     * This test is for verify to characters inverse inside of the word in a sentence.
+     */
+    @Test
+    public void verifyASentence() {
+        SortContent sortContent = new SortContent();
+        assertEquals("The Conidg Ptriccae", sortContent.sortContent("The Coding Practice"));
+    }
 }
