c617388da53b8ea089b54429addc75e3ba3bc917
Ovidio Miranda <Ovidio.Miranda@fundacion-jala.org>
2017-05-26 15:06:18 -0400

    Upload my code

--- src/main/java/org/fundacionjala/coding/ovidio_spinwords/SpinWord.java
+++ src/main/java/org/fundacionjala/coding/ovidio_spinwords/SpinWord.java
@@ -1,7 +1,48 @@
 package org.fundacionjala.coding.ovidio_spinwords;
 
+import java.util.StringJoiner;
+
 /**
  * Created by OvidioMiranda on 5/26/2017.
  */
+
 public class SpinWord {
+
+    private String sentence;
+
+
+    /**
+     * Constructor for objects of SpinWord.
+     */
+    public SpinWord() {
+        this("");
+    }
+
+    /**
+     * Constructor for objects of SpinWord with a parameter.
+     *
+     * @param sentence Is a word.
+     */
+    public SpinWord(String sentence) {
+        this.sentence = sentence;
+    }
+
+
+    /**
+     * Function that takes in a string and returns the same string
+     * but with all five or more letter words reversed.
+     *
+     * @return returns the same string but with all five or more letter words reversed .
+     */
+    public String spinWords() {
+        final Integer maxLetters = 5;
+        StringJoiner aux = new StringJoiner(" ");
+        String[] wordsArray = sentence.split(" ");
+        for (String word : wordsArray) {
+            aux = word.length() >= maxLetters ? aux.add(new StringBuilder(word).reverse()) : aux.add(word);
+        }
+        return aux.toString();
+    }
+
+
 }
--- src/test/java/org/fundacionjala/coding/ovidio_spinwords/SpinWordTest.java
+++ src/test/java/org/fundacionjala/coding/ovidio_spinwords/SpinWordTest.java
@@ -1,7 +1,52 @@
 package org.fundacionjala.coding.ovidio_spinwords;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by OvidioMiranda on 5/26/2017.
  */
 public class SpinWordTest {
+
+    /**
+     * Test for see one sentecen reverse.
+     */
+    @Test
+    public void whenYouHaveOneWord() {
+        SpinWord test1 = new SpinWord("Hello");
+        String result = test1.spinWords();
+        assertEquals("olleH", result);
+    }
+
+    /**
+     * Test for when you have three words and last string passed to five letters.
+     */
+    @Test
+    public void whenYouHaveThreeWordsAndLastStringPassedToFiveLetters() {
+        SpinWord test1 = new SpinWord("Hey fellow warriors");
+        String result = test1.spinWords();
+        assertEquals("Hey wollef sroirraw", result);
+    }
+
+    /**
+     * Test for when you have four words and are less to five letters.
+     */
+    @Test
+    public void whenYouHaveFourWordsAndAreLessToFiveLetters() {
+        SpinWord test1 = new SpinWord("This is a test");
+        String result = test1.spinWords();
+        assertEquals("This is a test", result);
+    }
+
+    /**
+     * Test for when you have three words and third word passed the five letters.
+     */
+    @Test
+    public void whenYouHaveThreeWordsAndThirdWordPassedTheFiveLetters() {
+        SpinWord test1 = new SpinWord("This is another test");
+        String result = test1.spinWords();
+        assertEquals("This is rehtona test", result);
+    }
+
 }
