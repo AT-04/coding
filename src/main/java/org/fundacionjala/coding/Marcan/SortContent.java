3cfbb6ea8d6a932161ace79134a265871779983b
Marcandea <marcandea2@gmail.com>
2017-05-26 16:53:16 -0400

    Finish first Test from SortContent

--- src/main/java/org/fundacionjala/coding/Marcan/SortContent.java
+++ src/main/java/org/fundacionjala/coding/Marcan/SortContent.java
@@ -1,7 +1,29 @@
 package org.fundacionjala.coding.Marcan;
 
+import java.util.Arrays;
+import java.util.StringJoiner;
+
 /**
  * Created by MarcandeaCabrera on 5/23/2017.
  */
 public class SortContent {
+    /**
+     * The following method is for reverse one word.
+     *
+     * @param string
+     * @return stringBuilder
+     */
+    public String sortContent(String string) {
+        String[] words = string.split(" ");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String word : words) {
+            char[] wordInChar = word.substring(1, word.length() - 1).toCharArray();
+            Arrays.sort(wordInChar);
+            stringBuilder.append(word.substring(word.length() - 1, word.length()));
+            stringBuilder.append(wordInChar);
+            stringBuilder.append(word.substring(0, 1));
+
+        }
+        return stringBuilder.reverse().toString();
+    }
 }
--- src/test/java/org/fundacionjala/coding/Marcan/SortContentTest.java
+++ src/test/java/org/fundacionjala/coding/Marcan/SortContentTest.java
@@ -1,7 +1,20 @@
 package org.fundacionjala.coding.Marcan;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by MarcandeaCabrera on 5/23/2017.
  */
 public class SortContentTest {
+    /**
+     * This test is for verify word inverse.
+     */
+    @Test
+    public void firstTest(){
+        SortContent sortContent=new SortContent();
+        assertEquals("Mrcaan",sortContent.sortContent("Marcan"));
+    }
+
 }
