3da2477d965a0422d82e840946dde9fb759389db
Ovidio Miranda <Ovidio.Miranda@fundacion-jala.org>
2017-05-26 12:51:07 -0400

    Initial comit for Sort Inner Content

--- src/test/java/org/fundacionjala/coding/Marcan/SortContentTest.java
+++
@@ -1,7 +0,0 @@
-package org.fundacionjala.coding.Marcan;
-
-/**
- * Created by MarcandeaCabrera on 5/23/2017.
- */
-public class SortContentTest {
-}
---
+++ src/test/java/org/fundacionjala/coding/ovidio_sortInnerContent/SortInnerContentTest.java
@@ -0,0 +1,7 @@
+package org.fundacionjala.coding.ovidio_sortInnerContent;
+
+/**
+ * Created by OvidioMiranda on 5/26/2017.
+ */
+public class SortInnerContentTest {
+}
