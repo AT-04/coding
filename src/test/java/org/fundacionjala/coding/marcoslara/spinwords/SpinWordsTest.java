b0455686ea7a5e63088a01517eb5b16cc5d355e2
Marcos Lara <marcos.x86@outlook.com>
2017-05-26 15:11:21 -0400

    Coding - Kata #1 - Spin Words (#10)
    * Coding - Kata #1 - Spin Words
    Initial commit.
    
    * Coding - Kata #1 - Spin Words Initial commit
    
    * Kata #1 - Spin Words Initial Commit
    
    * New solution for Spin Words.
    
    * New version of Spin Words solution

---
+++ src/test/java/org/fundacionjala/coding/marcoslara/spinwords/SpinWordsTest.java
@@ -0,0 +1,65 @@
+package org.fundacionjala.coding.marcoslara.spinwords;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Marcos Lara on 5/22/2017.
+ */
+public class SpinWordsTest {
+
+    /**
+     * This test proves that the result of spin spinning on an empty String is the same String.
+     */
+    @Test
+    public void getSpinWordsOfAEmptyStringIsAEmptyString() {
+        String word = "";
+        assertEquals("", SpinWords.spinWords(word));
+    }
+
+    /**
+     * This test proves that the result of spinning word on an short string (<=4 characters) is the same String.
+     */
+    @Test
+    public void getSpinWordsOfAShortStringIsTheSameString() {
+        String word = "abc";
+        assertEquals("abc", SpinWords.spinWords(word));
+    }
+
+    /**
+     * This test proves that the result of spinning word on an short string (<=4 characters) is the same String.
+     */
+    @Test
+    public void getSpinWordsOfAStringWithTwoWords() {
+        String word = "abc verify";
+        assertEquals("abc yfirev", SpinWords.spinWords(word));
+    }
+
+    /**
+     * This test proves the result of the spin words of the example string number one.
+     */
+    @Test
+    public void getSpinWordsOfATheExampleStringNumberOne() {
+        String word = "Hey fellow warriors";
+        assertEquals("Hey wollef sroirraw", SpinWords.spinWords(word));
+    }
+
+    /**
+     * This test proves the result of the spin words of the example string number two.
+     */
+    @Test
+    public void getSpinWordsOfATheExampleStringNumberTwo() {
+        String word = "This is a test";
+        assertEquals("This is a test", SpinWords.spinWords(word));
+    }
+
+    /**
+     * This test proves the result of the spin words of the example String number three.
+     */
+    @Test
+    public void getSpinWordsOfATheExampleStringNumberThree() {
+        String word = "This is another test";
+        assertEquals("This is rehtona test", SpinWords.spinWords(word));
+    }
+}
