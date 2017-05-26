d68b42fbe06fb87b9a29aab99fb58d7f623df390
Ovidio Miranda <Ovidio.Miranda@fundacion-jala.org>
2017-05-26 19:25:16 -0400

    Comit Two

--- src/main/java/org/fundacionjala/coding/ovidio_eanValidation/EanValidation.java
+++ src/main/java/org/fundacionjala/coding/ovidio_eanValidation/EanValidation.java
@@ -4,4 +4,12 @@ package org.fundacionjala.coding.ovidio_eanValidation;
  * Created by OvidioMiranda on 5/26/2017.
  */
 public class EanValidation {
+
+
+    public Boolean validate(String s) {
+
+
+
+        return true;
+    }
 }
--- src/test/java/org/fundacionjala/coding/ovidio_eanValidation/EanValidationTest.java
+++ src/test/java/org/fundacionjala/coding/ovidio_eanValidation/EanValidationTest.java
@@ -1,7 +1,23 @@
 package org.fundacionjala.coding.ovidio_eanValidation;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by OvidioMiranda on 5/26/2017.
  */
 public class EanValidationTest {
+
+
+    /*
+    *This test evaluate EAN
+     */
+    @Test
+    public void isEanValidate() {
+        EanValidation test = new EanValidation();
+        Boolean result = test.validate("4003301018398");
+        assertTrue(result);
+    }
 }
