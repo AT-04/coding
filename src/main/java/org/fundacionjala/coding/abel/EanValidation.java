013bf50b9475c787ed85776200a670d517b706ee
Abel Barrientos <abelbs27101@gmail.com>
2017-05-28 10:15:48 -0400

    First attempt to solve Kata 3

--- src/main/java/org/fundacionjala/coding/abel/EanValidation.java
+++ src/main/java/org/fundacionjala/coding/abel/EanValidation.java
@@ -1,36 +1,32 @@
 package org.fundacionjala.coding.abel;
 
-import java.util.stream.Stream;
 
 /**
  * Created by AbelBarrientos on 5/26/2017.
  */
 public final class EanValidation {
 
+    public static final int PAIR_POSITION = 3;
+    public static final int TEN = 10;
 
+    /**
+     * The constructor.
+     */
+    private EanValidation() { }
+
+    /**
+     * This method validate a checksum.
+     * @param checksum String to validate.
+     * @return boolean The result.
+     */
     public static boolean validate(String checksum) {
         char[] charArray = checksum.substring(0, checksum.length() - 1).toCharArray();
         int sum = 0;
         for (int i = 0; i < charArray.length; i++) {
             int intValue = Integer.parseInt(String.valueOf(charArray[i]));
-            sum += i % 2 == 0 ? intValue : intValue * 3;
+            sum += i % 2 == 0 ? intValue : intValue * PAIR_POSITION;
         }
-        int realChecksum = (sum % 10 == 0) ? 0 : 10 - (sum % 10);
+        int realChecksum = (sum % TEN == 0) ? 0 : TEN - (sum % TEN);
         return realChecksum == Integer.parseInt(checksum.substring(checksum.length() - 1));
-
-//        boolean flag = false;
-//        int sum2 = 0;
-//        Stream.of(checksum.substring(0, checksum.length() - 1).split(""))
-//                .map((character, index) -> {
-//                    int intValue = Integer.parseInt(character);
-//                    sum2 += (index % 2 == 0) ? intValue * 3 : intValue;
-//                    flag = !flag;
-////                    if (flag)
-////                        sum2 += intValue * 3;
-////                    else
-////                        sum2 += intValue;
-//                });
-//        int realChecksum = (sum2 % 10 == 0) ? 0 : 10 - (sum2 % 10);
-//        return realChecksum == Integer.parseInt(checksum.substring(checksum.length() - 1));
     }
 }
