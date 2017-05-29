4177753e3cf7b96946a32fb5fddad3570f983056
Gioserden <sergio.navarro@fundacion-jala.org>
2017-05-28 20:39:49 -0400

    Spin words with observation.

---
+++ src/main/java/org/fundacionjala/coding/sergio_spin_words/Spin_words.java
@@ -0,0 +1,7 @@
+package org.fundacionjala.coding.sergio_spin_words;
+
+/**
+ * Created by Sergio on 28/05/2017.
+ */
+public class spin_words {
+}
---
+++ src/test/java/org/fundacionjala/coding/sergio_spin_words/spin_wordsTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sergio on 28/05/2017.
+ */
+public class spin_wordsTest {
+
+}
