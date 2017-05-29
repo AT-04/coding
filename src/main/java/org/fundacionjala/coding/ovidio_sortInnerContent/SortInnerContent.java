b072e1d322c31a35b82b019e627f4c9e1e674ad0
Ovidio Miranda <Ovidio.Miranda@fundacion-jala.org>
2017-05-29 19:06:35 -0400

    Code finished for sort inner content.

--- src/main/java/org/fundacionjala/coding/ovidio_sortInnerContent/SortInnerContent.java
+++ src/main/java/org/fundacionjala/coding/ovidio_sortInnerContent/SortInnerContent.java
@@ -1,7 +1,48 @@
 package org.fundacionjala.coding.ovidio_sortInnerContent;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringJoiner;
+
 /**
  * Created by OvidioMiranda on 5/26/2017.
  */
 public class SortInnerContent {
+
+    /**
+     * Function the sort the inner content of every word of a string in descending
+     * order The inner content is the content of a word without first and last char.
+     *
+     * @param sentence Is a sentence with words.
+     * @return returns the string but sort the inner content of every word in descending order.
+     */
+    public String sort(String sentence) {
+        StringJoiner aux = new StringJoiner(" ");
+        String[] wordsArray = sentence.split(" ");
+        for (String word : wordsArray) {
+            aux.add(sorTheInnerContent(word));
+        }
+        return aux.toString();
+    }
+
+    /**
+     * Function the sort the inner content of only word of  in descending order without
+     * first and last char.
+     *
+     * @param word Is a words.
+     * @return returns the string but sort the inner content of every word in descending order.
+     */
+    public static String sorTheInnerContent(String word) {
+        final int positionLastLetter = word.length() - 1;
+        final String firstLetter = word.substring(0, 1);
+        final String lastLetter = word.substring(positionLastLetter);
+        String[] wordDescendingOrder = word.substring(1, positionLastLetter).split("");
+        Arrays.sort(wordDescendingOrder, Collections.reverseOrder());
+        StringBuilder newString = new StringBuilder(firstLetter);
+        newString.append(String.join("", wordDescendingOrder)).append(lastLetter);
+        return newString.toString();
+    }
+
+
 }
+
--- src/test/java/org/fundacionjala/coding/ovidio_sortInnerContent/SortInnerContentTest.java
+++ src/test/java/org/fundacionjala/coding/ovidio_sortInnerContent/SortInnerContentTest.java
@@ -1,7 +1,53 @@
 package org.fundacionjala.coding.ovidio_sortInnerContent;
 
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 /**
  * Created by OvidioMiranda on 5/26/2017.
  */
 public class SortInnerContentTest {
+
+    /**
+     * Will be tested the sorted with only a word.
+     */
+    @Test
+    public void sortedWithOnlyWord() {
+        SortInnerContent sortInnerContent = new SortInnerContent();
+        assertEquals("ctonnet", sortInnerContent.sort("content"));
+    }
+
+    /**
+     * Will be tested the sorted with six words.
+     */
+    @Test
+    public void sortedWithSixWords() {
+        SortInnerContent sortInnerContent = new SortInnerContent();
+        String sentence = "sort the inner content in descending order";
+        assertEquals("srot the inner ctonnet in dsnnieedcg oredr", sortInnerContent.sort(sentence));
+    }
+
+    /**
+     * Will be tested the sorted with three words.
+     */
+    @Test
+    public void sortedWithThreeWords() {
+        SortInnerContent sortInnerContent = new SortInnerContent();
+        String sentence = "wait for me";
+        assertEquals("wiat for me", sortInnerContent.sort(sentence));
+    }
+
+    /**
+     * Will be tested the sorted with four words.
+     */
+    @Test
+    public void sortedWithFourWords() {
+        SortInnerContent sortInnerContent = new SortInnerContent();
+        String sentence = "this kata is easy";
+        assertEquals("tihs ktaa is esay", sortInnerContent.sort(sentence));
+    }
+
 }
