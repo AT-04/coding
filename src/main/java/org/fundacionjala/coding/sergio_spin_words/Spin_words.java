d02e81d3f91da038fddec9c25ae2faf4545af306
Gioserden <sergio.navarro@fundacion-jala.org>
2017-05-28 20:43:18 -0400

    Commit Test

--- src/main/java/org/fundacionjala/coding/sergio_spin_words/Spin_words.java
+++ src/main/java/org/fundacionjala/coding/sergio_spin_words/Spin_words.java
@@ -3,5 +3,30 @@ package org.fundacionjala.coding.sergio_spin_words;
 /**
  * Created by Sergio on 28/05/2017.
  */
-public class spin_words {
+public final class Spin_words {
+
+    private static final int CONDITION = 5;
+
+    /**
+     * CONSTRUCTOR.
+     */
+    private Spin_words() {
+
+    }
+
+    /**
+     *
+     * @param sentence IS THE SENTENCE ENTRY.
+     * @return THE SENTENCE WITH WORD > 5 SPINNED.
+     */
+
+    public static String spinWord(String sentence) {
+        String[] stringWords = sentence.trim().split(" ");
+        for (int i = 0; i < stringWords.length; i++) {
+            if (stringWords[i].length() >= CONDITION) {
+                stringWords[i] = new StringBuilder(stringWords[i]).reverse().toString();
+            }
+        }
+        return String.join(" ", stringWords);
+    }
 }
--- src/test/java/org/fundacionjala/coding/sergio_spin_words/spin_wordsTest.java
+++ src/test/java/org/fundacionjala/coding/sergio_spin_words/spin_wordsTest.java
@@ -1,8 +1,35 @@
+package org.fundacionjala.coding.sergio_spin_words;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by Sergio on 28/05/2017.
  */
 public class spin_wordsTest {
+    @Test
+    public void spinOneWord() {
+        String sentence = "warrior";
 
+        assertEquals("roirraw", Spin_words.spinWord(sentence));
+    }
+    @Test
+    public void sameWordsIsWordsLengthIsMinorToFive() {
+        String sentence = "It is a Test";
+
+        assertEquals("It is a Test", Spin_words.spinWord(sentence));
+    }
+    @Test
+    public void sentenceIsEmptyReturnEmpty() {
+        String sentence = "";
+
+        assertEquals("", Spin_words.spinWord(sentence));
+    }
+    @Test
+    public void sentenceHaveASpaceAtTheBeginningReturnWithoutSpace() {
+        String sentence = " Sentence with a space";
+
+        assertEquals("ecnetneS with a ecaps", Spin_words.spinWord(sentence));
+    }
 }
