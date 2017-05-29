a15860799d60637f6dff32757401c3512dbf06f7
Marco Mendez <marco.mendez@fundacion-jala.org>
2017-05-29 11:53:56 +0000

    finish.SpinWord.marcomendez

--- src/main/java/org/fundacionjala/coding/MarcoM_spin_word/SpinWorld.java
+++ src/main/java/org/fundacionjala/coding/MarcoM_spin_word/SpinWorld.java
@@ -4,4 +4,29 @@ package org.fundacionjala.coding.MarcoM_spin_word;
  * Created by Administrator on 5/29/2017.
  */
 public class SpinWorld {
+    public static final int CONSTANT = 5;
+    /**
+     * verify.
+     *
+     * @param sentence hello world.
+     * @return String
+     */
+    public String reverse(String sentence) {
+        if (sentence.length() == 0) {
+            return "";
+        }
+        String[] words = sentence.split(" ");
+        StringBuilder aux = new StringBuilder();
+
+        for (String word : words) {
+            if (word.length() >= CONSTANT) {
+                aux.append(" ");
+                StringBuilder builder = new StringBuilder(word);
+                aux.append(builder.reverse().toString());
+            }
+            aux.append(" ");
+            aux.append(word);
+        }
+        return aux.toString().trim();
+    }
 }
--- src/test/java/org/fundacionjala/coding/MarcoM_spin_word/TestSpinWord.java
+++ src/test/java/org/fundacionjala/coding/MarcoM_spin_word/TestSpinWord.java
@@ -1,7 +1,28 @@
 package org.fundacionjala.coding.MarcoM_spin_word;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Administrator on 5/29/2017.
  */
 public class TestSpinWord {
+    /**
+     * verify.
+     */
+    @Test
+    public void returnWord() {
+        SpinWorld k = new SpinWorld();
+        assertEquals("abd j", k.reverse("abd j"));
+    }
+
+    /**
+     *verify.
+     */
+    @Test
+    public void returnWordTwo() {
+        SpinWorld k = new SpinWorld();
+        assertEquals("hola mund", k.reverse("hola mund"));
+    }
 }
