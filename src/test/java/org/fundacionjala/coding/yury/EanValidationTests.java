2c6db555536473f566e221df2cd25a4e9f957bd8
John Doe <yveryury@gmail.com>
2017-05-30 10:53:35 -0400

    finish EAN validation

--- src/test/java/org/fundacionjala/coding/yury/EanValidationTests.java
+++ src/test/java/org/fundacionjala/coding/yury/EanValidationTests.java
@@ -1,7 +1,32 @@
 package test.java.org.fundacionjala.coding.yury;
 
+import main.java.org.fundacionjala.coding.yury.EanValidation;
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by YuryOrtuno on 5/26/2017.
  */
 public class EanValidationTests {
+
+    /**
+     *
+     */
+    @Test
+    public void timesNumber() {
+        EanValidation eanValidation = new EanValidation();
+        assertTrue(eanValidation.validation("4003301018398"));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void timesNumberIsFalse() {
+        EanValidation eanValidation = new EanValidation();
+        assertFalse(eanValidation.validation("4003301018392"));
+    }
+
+
 }
