8f4f4a6031e3304ae93ec1bef9a074355268997e
Marcos Lara <marcos.x86@outlook.com>
2017-05-30 16:07:30 -0400

    Marcos lara ean validation (#29)
    * Coding - Katas #3 EAN Validator
    
    * Coding - Katas #3 EAN Validator
    
    * Corrected code.

---
+++ src/main/java/org/fundacionjala/coding/marcoslara/eanvalidation/EanValidator.java
@@ -0,0 +1,33 @@
+package org.fundacionjala.coding.marcoslara.eanvalidation;
+
+/**
+ * Created by Marcos Lara on 5/26/2017.
+ */
+public final class EanValidator {
+
+    private static final int NUMBER_OF_DIGITS = 13;
+    private static final int MODULE_DIVIDEND = 10;
+    private static final int EAN_MULTIPLIER = 3;
+
+    /**
+     * Private constructor required. These kind of classes are more like tools/utilities than being meant for objects.
+     */
+    private EanValidator() {
+
+    }
+
+    /**
+     * @param eanCode Is the code represented by a String object that will be validated.
+     * @return Returns the result of the validation (true or false).
+     */
+    public static boolean validate(String eanCode) {
+        char[] digits = eanCode.toCharArray();
+        int checksum = 0;
+        for (int i = 0; i < NUMBER_OF_DIGITS - 1; i++) {
+            int digit = Character.getNumericValue(digits[i]);
+            checksum += (i + 1) % 2 == 0 ? digit * EAN_MULTIPLIER : digit;
+        }
+        checksum = checksum % MODULE_DIVIDEND == 0 ? 0 : MODULE_DIVIDEND - (checksum % MODULE_DIVIDEND);
+        return Character.getNumericValue(digits[NUMBER_OF_DIGITS - 1]) == checksum;
+    }
+}
