f758e2e6415636d5613968ffda28bd4bfa932154
Ovidio Miranda <Ovidio.Miranda@fundacion-jala.org>
2017-05-30 15:50:42 -0400

    Improving the code.

--- src/main/java/org/fundacionjala/coding/ovidio_eanValidation/EanValidation.java
+++ src/main/java/org/fundacionjala/coding/ovidio_eanValidation/EanValidation.java
@@ -1,6 +1,8 @@
 package org.fundacionjala.coding.ovidio_eanValidation;
 
 
+import java.util.Arrays;
+
 /**
  * Created by OvidioMiranda on 5/26/2017.
  */
@@ -16,7 +18,7 @@ public class EanValidation {
      * @return bool Return true if the given EAN-Code is valid, otherwise false.
      */
     public Boolean validate(String eanCode) {
-        int result = 0;
+        int result;
         int calcChecksum;
         int positionLastNumber;
         int lastNumber;
@@ -25,9 +27,7 @@ public class EanValidation {
         String twelveDigits = eanCode.substring(0, positionLastNumber);
         int[] digits;
         digits = twelveDigits.chars().map(c -> c -= '0').toArray();
-        for (int d : digits) {
-            result += d % 2 == 0 ? d : d * TREE;
-        }
+        result = Arrays.stream(digits).map(d -> d % 2 == 0 ? d : d * TREE).sum();
         calcChecksum = (result % TEN == 0) ? 0 : TEN - (result % TEN);
         return calcChecksum == lastNumber;
     }
