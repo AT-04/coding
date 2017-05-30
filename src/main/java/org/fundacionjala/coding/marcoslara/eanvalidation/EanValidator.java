c6efb17f2978c6922196d19b4add4cfd02be1ea6
marcos-x86 <marcos.x86@outlook.com>
2017-05-30 13:41:32 -0400

    Coding - Katas #3 EAN Validator

--- src/main/java/org/fundacionjala/coding/marcoslara/eanvalidation/EanValidator.java
+++ src/main/java/org/fundacionjala/coding/marcoslara/eanvalidation/EanValidator.java
@@ -1,7 +1,34 @@
 package org.fundacionjala.coding.marcoslara.eanvalidation;
 
 /**
- * Created by Administrator on 5/26/2017.
+ * Created by Marcos Lara on 5/26/2017.
  */
-public class EanValidator {
+public final class EanValidator {
+
+    private static final int NUMBER_OF_DIGITS = 13;
+    private static final int MODULE_DIVIDEND = 10;
+    private static final int EAN_MULTIPLIER = 3;
+    private static final int ZERO_ASCII = 48;
+
+    /**
+     * Private constructor required. These kind of classes are more like tools/utilities than being meant for objects.
+     */
+    private EanValidator() {
+
+    }
+
+    /**
+     * @param eanCode Is the code represented by a String object that will be validated.
+     * @return Returns the result of the validation (true or false).
+     */
+    public static boolean validate(String eanCode) {
+        char[] digits = eanCode.toCharArray();
+        int checksum = 0;
+        for (int i = 0; i < NUMBER_OF_DIGITS - 1; i++) {
+            checksum += (i + 1) % 2 == 0 ? (digits[i] - ZERO_ASCII) * EAN_MULTIPLIER : digits[i] - ZERO_ASCII;
+        }
+        checksum = checksum % MODULE_DIVIDEND == 0 ? 0 : MODULE_DIVIDEND - (checksum % MODULE_DIVIDEND);
+        digits[NUMBER_OF_DIGITS - 1] = (char) (checksum + ZERO_ASCII);
+        return eanCode.equals(new String(digits));
+    }
 }
--- src/test/java/org/fundacionjala/coding/marcoslara/eanvalidation/EanValidatorTest.java
+++ src/test/java/org/fundacionjala/coding/marcoslara/eanvalidation/EanValidatorTest.java
@@ -1,7 +1,31 @@
 package org.fundacionjala.coding.marcoslara.eanvalidation;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
- * Created by Administrator on 5/26/2017.
+ * Created by Marcos Lara on 5/26/2017.
  */
 public class EanValidatorTest {
+    /**
+     * Test of a valid EAN code.
+     */
+    @Test
+    public void eanValidationOfaStringExample1() {
+        String eanCode = "4003301018398";
+
+        assertTrue(EanValidator.validate(eanCode));
+    }
+
+    /**
+     * Test of a invalid EAN code.
+     */
+    @Test
+    public void eanValidationOfaStringExample2() {
+        String eanCode = "4003301018392";
+
+        assertFalse(EanValidator.validate(eanCode));
+    }
 }
