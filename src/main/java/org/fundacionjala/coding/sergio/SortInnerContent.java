22d4547ef363edfc8ba15dfbf4b885ba2de657e5
Gioserden <sergio.navarro@fundacion-jala.org>
2017-06-01 17:06:34 -0400

    First attempt for kata version 2

--- src/main/java/org/fundacionjala/coding/sergio/SortInnerContent.java
+++ src/main/java/org/fundacionjala/coding/sergio/SortInnerContent.java
@@ -1,5 +1,6 @@
 package org.fundacionjala.coding.sergio;
 
+import java.util.Comparator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -8,21 +9,47 @@ import java.util.stream.Stream;
  */
 public class SortInnerContent {
 
-    public static final int SIZE = 3;
 
-    public static String sortReverse(String sentence) {
-        return Stream.of(sentence.split(" "))
-                .map(word -> (word.equals(null) || word.equals("")) ? null :"")
+    public static String[] sortReverse(String sentence) {
+        String[] cadenas = sentence.split(" ");
+        for (String ele: cadenas) {
+            if(ele.length()>3) {
+                ele = SortInnerContent.sort(ele);
+            }
+        }
+        return cadenas;
+    }
+
+    private static String sort(String ele) {
+        String sorted1 = Stream.of(ele.substring(1, ele.length()-1))
+        .sorted(Comparator.reverseOrder())
                 .collect(Collectors.joining());
-
-                }
-                //.map(word-> (word == null || word == "") ? null : ""0);
-
-
-        //return "hi all floks";
+        return String.join("", ele.substring(0, 1), sorted1, ele.substring(ele.length()-1, ele.length()));
     }
 
-    private static void sort(String element) {
+
+    /**private static void sort(String word) {
+        Stream.of(word)
+                .sorted(Comparator.reverseOrder());
+    }
+    /**return Stream.of(sentence.split(" "))
+                .map(word -> (word.length() >= SIZE) ?SortInnerContent.sort(word.substring(1, word.length()-1).)
+                .collect(Collectors.joining());**/
 
     }
-}
+
+    /**private static boolean validation(String sentence) {
+        String[] cadenas = sentence.split(" ");
+        for (String element : cadenas) {
+            if(element.toUpperCase() == true) {
+                return true;
+            }
+
+        }
+        return true;
+    }**/
+
+
+
+
+
