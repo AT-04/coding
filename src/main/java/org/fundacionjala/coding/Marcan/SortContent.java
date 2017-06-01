a079b87da39e40956c89f0b3ad7ac1aa002c634b
Marcandea <marcandea2@gmail.com>
2017-06-01 19:21:05 -0400

    Marcandea.finish.SortContent.Corrected

--- src/main/java/org/fundacionjala/coding/Marcan/SortContent.java
+++ src/main/java/org/fundacionjala/coding/Marcan/SortContent.java
@@ -1,4 +1,4 @@
-package org.fundacionjala.coding.Marcan;
+package org.fundacionjala.coding.marcan;
 
 import java.util.Arrays;
 import java.util.StringJoiner;
@@ -7,6 +7,9 @@ import java.util.StringJoiner;
  * Created by MarcandeaCabrera on 5/23/2017.
  */
 public class SortContent {
+
+    public static final int THREE = 3;
+
     /**
      * The following method is for reverse one word.
      *
@@ -17,14 +20,18 @@ public class SortContent {
         String[] words = string.split(" ");
         StringJoiner stringJoiner = new StringJoiner(" ");
         for (String word : words) {
-            char[] wordInChar = word.substring(1, word.length() - 1).toCharArray();
-            Arrays.sort(wordInChar);
-            StringBuilder str = new StringBuilder();
-            str.append(word.substring(word.length() - 1, word.length()));
-            str.append(wordInChar);
-            str.append(word.substring(0, 1));
-            String reversed = str.reverse().toString();
-            stringJoiner.add(reversed);
+            if (word.length() < THREE) {
+                stringJoiner.add(word);
+            } else {
+                char[] wordInChar = word.substring(1, word.length() - 1).toCharArray();
+                Arrays.sort(wordInChar);
+                StringBuilder str = new StringBuilder();
+                str.append(word.substring(word.length() - 1, word.length()));
+                str.append(wordInChar);
+                str.append(word.substring(0, 1));
+                String reversed = str.reverse().toString();
+                stringJoiner.add(reversed);
+            }
         }
         return stringJoiner.toString();
     }
--- src/test/java/org/fundacionjala/coding/Marcan/SortContentTest.java
+++ src/test/java/org/fundacionjala/coding/Marcan/SortContentTest.java
@@ -1,5 +1,6 @@
-package org.fundacionjala.coding.Marcan;
+package org.fundacionjala.coding.marcan;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,13 +9,23 @@ import static org.junit.Assert.assertEquals;
  * Created by MarcandeaCabrera on 5/23/2017.
  */
 public class SortContentTest {
+
+    private SortContent sortContent;
     /**
-     * This test is for verify to characters inverse inside of the word.
+     * This test is a method Before.
+     */
+    @Before
+    public void methodBefore() {
+        sortContent = new SortContent();
+    }
+
+    /**
+     * This test is for verify to characters inverse inside of the word in a sentence.
      */
     @Test
     public void verifyOneWord() {
-        SortContent sortContent = new SortContent();
         assertEquals("Conidg", sortContent.sortContent("Coding"));
+        assertEquals("I am", sortContent.sortContent("I am"));
     }
 
     /**
@@ -22,7 +33,24 @@ public class SortContentTest {
      */
     @Test
     public void verifyASentence() {
-        SortContent sortContent = new SortContent();
         assertEquals("The Conidg Ptriccae", sortContent.sortContent("The Coding Practice"));
+    }
+
+    /**
+     * This test is for verify to characters inverse inside of the word in a sentence.
+     * Different Stage, less to three characters.
+     */
+    @Test
+    public void verifyWordsLessToThreeCharacters() {
+        assertEquals("I am", sortContent.sortContent("I am"));
+    }
+
+    /**
+     * This test is for verify to characters inverse inside of the word in a sentence.
+     * Best stage. Combination stage.
+     */
+    @Test
+    public void verifyWordsLessAndGreaterToThreeCharacters() {
+        assertEquals("I am Conidg", sortContent.sortContent("I am Coding"));
     }
 }
