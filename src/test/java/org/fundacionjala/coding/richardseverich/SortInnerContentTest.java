3a203986e5553d94a52300e544c89c8afec2127f
RichardSeverich <richard.severich@fundacion-jala.org>
2017-06-03 00:32:44 +0000

    Richard Severich the Kata 2 Sort Inner Content. (#15)
    * This is a commit of the Kata 2 Sort Inner Content exercise.
    
    * This is a second commit of Sort Inner Content
    
    * Commit Richard Severich
    
    * commit Richard Severich
    
    * Commit Richard Severich
    
    * Commit Richard Severich
    
    * Commit by Richard Severich Sort Inner Content
    
    * commit by Richard Severich of Sort Inner Content
    
    * Final Commit by Richard Severich Sort Inner Content

---
+++ src/test/java/org/fundacionjala/coding/richardseverich/SortInnerContentTest.java
@@ -0,0 +1,63 @@
+package org.fundacionjala.coding.richardseverich;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by RichardSeverich on 5/23/2017.
+ */
+public class SortInnerContentTest {
+
+    private SortInnerContent sortInner;
+
+    /**
+     * This is the method that will run before each unit test.
+     */
+    @Before
+    public void setUp() {
+        sortInner = new SortInnerContent();
+    }
+
+    /**
+     * This test checks the string that returns the method getSortInner returns the expected string.
+     */
+    @Test
+    public void verifyChangeOfWaitForMe() {
+        String firstCad = "wait for me";
+        String expectedResult = "wiat for me";
+        assertEquals(expectedResult, sortInner.sortInner(firstCad));
+    }
+
+    /**
+     * This test checks the string that returns the method getSortInner returns the expected string.
+     */
+    @Test
+    public void verifyChangeOfThisKataIsEasy() {
+        String firstCad = "this kata is easy";
+        String expectedResult = "tihs ktaa is esay";
+        assertEquals(expectedResult, sortInner.sortInner(firstCad));
+    }
+
+    /**
+     * This test checks the string that returns the method getSortInner returns the expected string.
+     */
+    @Test
+    public void verifyChangeOfSortTheInnerContentInDescendingOrder() {
+        String firstCad = "sort the inner content in descending order";
+        String expectedResult = "srot the inner ctonnet in dsnnieedcg oredr";
+        assertEquals(expectedResult, sortInner.sortInner(firstCad));
+    }
+
+    /**
+     * This test checks the string that returns the method getSortInner returns the expected string.
+     */
+    @Test
+    public void verityChangeOfSortTheInnerContentInDescendingOrderIam() {
+        String firstCad = "I am";
+        String expectedResult = "I am";
+        assertEquals(expectedResult, sortInner.sortInner(firstCad));
+    }
+
+}
