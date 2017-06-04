19e79c46771c4e8b10657ad85e7cefeeb9768c91
Gioserden <sergio.navarro@fundacion-jala.org>
2017-06-04 15:44:13 -0400

    First try of EAN Validation.

---
+++ src/main/java/org/fundacionjala/coding/sergio/EanValidation.java
@@ -0,0 +1,36 @@
+package org.fundacionjala.coding.sergio;
+
+/**
+ * Created by Sergio on 04/06/2017.
+ */
+public final class EanValidation {
+
+    private static final int VAL = 3;
+    private static final int CONDITION = 10;
+
+    /**
+     * Default.
+     */
+    private EanValidation() {
+
+    }
+
+    /**
+     *
+     * @param entry any value of user entry.
+     * @return CheckSum validation.
+     */
+    public static boolean validation(String entry) {
+        String[] digits = entry.substring(0, entry.length() - 1).split("");
+        int evenOddSum = 0;
+        for (int i = 0; i < entry.length() - 1; i++) {
+            if (i % 2 != 0) {
+                evenOddSum += Integer.parseInt(digits[i]) * VAL;
+            } else {
+                evenOddSum += Integer.parseInt(digits[i]);
+            }
+        }
+        evenOddSum = CONDITION - (evenOddSum % CONDITION);
+        return evenOddSum == Integer.parseInt(entry.substring(entry.length() - 1, entry.length()));
+    }
+}
