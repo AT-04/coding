57beff0864271ed095c60a2bd83063407d473c51
cynthia terrazas gutierrez <star_ok@hotmail.com>
2017-06-05 00:16:41 -0400

    solution banck OCR

--- src/main/java/org/fundacionjala/coding/cynthia/BankOCR.java
+++ src/main/java/org/fundacionjala/coding/cynthia/BankOCR.java
@@ -1,13 +1,94 @@
 package org.fundacionjala.coding.cynthia;
 
+import java.util.HashMap;
+import java.util.Map;
 /**
- * Created by CynthiaTerrazas on 5/16/2017.
+ * Created by CynthiaTerrazas on 6/04/2017.
  */
-public class BankOCR {
+public final class BankOCR {
+    private static final int ZERO = 0;
+    private static final int ONE = 1;
+    private static final int TWO = 2;
+    private static final int THREE = 3;
+    private static final int FOUR = 4;
+    private static final int FIVE = 5;
+    private static final int SIX = 6;
+    private static final int SEVEN = 7;
+    private static final int EIGHT = 8;
+    private static final int NINE = 9;
+    private static final int NUMBER_DIGITS = 9;
+    private static final int FIRST_LINE = 27;
+    private static final int SECOND_LINE = 54;
+    private static final int LONG_LINE = 3;
+    private static final int MODULO = 11;
+    private static final Map<String, Integer> STRING_NUMBERS = new HashMap<String, Integer>() {
+        {
+            put(" _ | ||_|", ZERO);
+            put("     |  |", ONE);
+            put(" _  _||_ ", TWO);
+            put(" _  _| _|", THREE);
+            put("   |_|  |", FOUR);
+            put(" _ |_  _|", FIVE);
+            put(" _ |_ |_|", SIX);
+            put(" _   |  |", SEVEN);
+            put(" _ |_||_|", EIGHT);
+            put(" _ |_| _|", NINE);
+        }
+    };
 
-    public static int convertEntryToNumber(String entryLine1, String entryLine2, String entryLine3) {
-        return 123456789;
+    /**
+     * Constructor.
+     */
+    private BankOCR() {
+    }
+
+    /**
+     * @param stringNumber string with the code of the number for example(" _ |_| _|")
+     * @return String the value the number
+     */
+    public static String convertNumber(String stringNumber) {
+        String firstLine = stringNumber.substring(0, FIRST_LINE);
+        String secondLine = stringNumber.substring(FIRST_LINE, SECOND_LINE);
+        String thirdLine = stringNumber.substring(SECOND_LINE);
+        int a = 0;
+        int b = LONG_LINE;
+        String number = "";
+        for (int i = 1; i <= NUMBER_DIGITS; i++) {
+            String digit = firstLine.substring(a, b) + secondLine.substring(a, b) + thirdLine.substring(a, b);
+            a = b;
+            b = b + LONG_LINE;
+            number = number + getNumberValue(digit);
+        }
+        return number;
     }
 
 
+    /**
+     * @param digit code of the number for example(" _ |_| _|")
+     * @return String the value the number
+     */
+    private static String getNumberValue(String digit) {
+        Integer number = STRING_NUMBERS.get(digit);
+        return number == null ? "?" : number.toString();
+    }
+
+    /**
+     * @param number number convert.
+     * @return String if is valid number return the number if not return number with "ERR".
+     */
+    private static String validNumber(String number) {
+        int checksum = 0;
+        for (int i = 1; i <= number.length(); i++) {
+            checksum = checksum + (Integer.parseInt("" + number.charAt(number.length() - i))) * i;
+        }
+        return checksum % MODULO == 0 ? number : number + " ERR";
+    }
+
+    /**
+     * @param number number convert.
+     * @return String if contain "?" return number with "ILL" if not number or Number with "ERR"
+     */
+    public static String output(String number) {
+        return number.contains("?") ? number + " ILL" : validNumber(number);
+    }
 }
--- src/test/java/org/fundacionjala/coding/cynthia/BankOCRTest.java
+++ src/test/java/org/fundacionjala/coding/cynthia/BankOCRTest.java
@@ -1,36 +1,184 @@
 package org.fundacionjala.coding.cynthia;
 
-import org.fundacionjala.coding.cynthia.BankOCR;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static junit.framework.TestCase.assertEquals;
+import static org.fundacionjala.coding.cynthia.BankOCR.convertNumber;
+import static org.fundacionjala.coding.cynthia.BankOCR.output;
 
 /**
  * Created by CynthiaTerrazas on 5/16/2017.
  */
 public class BankOCRTest {
+    /**
+     * this is the first test with numbers 123456789.
+     */
     @Test
-    public void bankOCRtoTry(){
+    public void bankOCRtoTryWith9DiferentesNumbers() {
+        String entryLine = "    _  _     _  _  _  _  _ "
+                + "  | _| _||_||_ |_   ||_||_|"
+                + "  ||_  _|  | _||_|  ||_| _|";
 
-        String entryLine1 = "    _  _     _  _  _  _  _ ";
-        String entryLine2 = "  | _| _||_||_ |_   ||_||_|";
-        String entryLine3 = "  ||_  _|  | _||_|  ||_| _|";
-
-        String expectedResult = "123456789";
-
-        assertEquals(expectedResult, BankOCR.convertEntryToNumber(entryLine1, entryLine2, entryLine3));
-
+        assertEquals("123456789", convertNumber(entryLine));
     }
 
+    /**
+     * this test is for just zeros.
+     */
     @Test
-    public void theDigitIsOne(){
-        String entryLine1 ="   ";
-        String entryLine2 ="  |";
-        String entryLine3 ="  |";
+    public void testBankOCRForJustZeros() {
+        String entryLine = " _  _  _  _  _  _  _  _  _ "
+                + "| || || || || || || || || |"
+                + "|_||_||_||_||_||_||_||_||_|";
 
+        assertEquals("000000000", convertNumber(entryLine));
     }
 
+    /**
+     * this test is for just one.
+     */
+    @Test
+    public void testBankOCRSForJustOnes() {
+        String entryLine = "                           "
+                + "  |  |  |  |  |  |  |  |  |"
+                + "  |  |  |  |  |  |  |  |  |";
+
+        assertEquals("111111111", convertNumber(entryLine));
+    }
+
+    /**
+     * this test is for just for two.
+     */
+    @Test
+    public void testBankOCRSForJustTwos() {
+        String entryLine = " _  _  _  _  _  _  _  _  _ "
+                + " _| _| _| _| _| _| _| _| _|"
+                + "|_ |_ |_ |_ |_ |_ |_ |_ |_ ";
+
+        assertEquals("222222222", convertNumber(entryLine));
+    }
+
+    /**
+     * this test is for just three.
+     */
+    @Test
+    public void testBankOCRForJustThrees() {
+        String entryLine = " _  _  _  _  _  _  _  _  _ "
+                + " _| _| _| _| _| _| _| _| _|"
+                + " _| _| _| _| _| _| _| _| _| ";
+
+        assertEquals("333333333", convertNumber(entryLine));
+    }
+
+    /**
+     * this test is for just four.
+     */
+    @Test
+    public void testBankOCRForJustFours() {
+        String entryLine = "                           "
+                + "|_||_||_||_||_||_||_||_||_|"
+                + "  |  |  |  |  |  |  |  |  |";
+
+        assertEquals("444444444", convertNumber(entryLine));
+    }
+
+    /**
+     * this test is for just five.
+     */
+    @Test
+    public void testBankOCRForJustFives() {
+        String entryLine = " _  _  _  _  _  _  _  _  _ "
+                + "|_ |_ |_ |_ |_ |_ |_ |_ |_ "
+                + " _| _| _| _| _| _| _| _| _|";
+
+        assertEquals("555555555", convertNumber(entryLine));
+    }
+
+    /**
+     * this test is for just six.
+     */
+    @Test
+    public void testBankOCRForJustSixs() {
+        String entryLine = " _  _  _  _  _  _  _  _  _ "
+                + "|_ |_ |_ |_ |_ |_ |_ |_ |_ "
+                + "|_||_||_||_||_||_||_||_||_|";
+
+        assertEquals("666666666", convertNumber(entryLine));
+    }
+
+    /**
+     * this test is for just seven.
+     */
+    @Test
+    public void testBankOCRForJustSevens() {
+        String entryLine = " _  _  _  _  _  _  _  _  _ "
+                + "  |  |  |  |  |  |  |  |  |"
+                + "  |  |  |  |  |  |  |  |  |";
+
+        assertEquals("777777777", convertNumber(entryLine));
+    }
+
+    /**
+     * this test is for just eight.
+     */
+    @Test
+    public void testBankOCRForJustEights() {
+        String entryLine = " _  _  _  _  _  _  _  _  _ "
+                + "|_||_||_||_||_||_||_||_||_|"
+                + "|_||_||_||_||_||_||_||_||_|";
+
+        assertEquals("888888888", convertNumber(entryLine));
+    }
+
+    /**
+     * this test is for just nine.
+     */
+    @Test
+    public void testBankOCRForJustNines() {
+        String entryLine = " _  _  _  _  _  _  _  _  _ "
+                + "|_||_||_||_||_||_||_||_||_|"
+                + " _| _| _| _| _| _| _| _| _|";
+
+        assertEquals("999999999", convertNumber(entryLine));
+    }
+
+    /**
+     * this test is for correct number count .
+     */
+    @Test
+    public void testBankOCRForCorrectNumberCount() {
+        String entryLine = "    _  _  _  _  _  _  _  _ "
+                + "|_||_   ||_ | ||_|| || || |"
+                + "  | _|  | _||_||_||_||_||_|";
+
+        assertEquals("457508000", output(convertNumber(entryLine)));
+    }
+
+    /**
+     * this test is for incorrect number count .
+     */
+    @Test
+    public void testBankOCRForIncorrectNumberCount() {
+        String entryLine = " _  _     _  _        _  _ "
+                + "|_ |_ |_| _|  |  ||_||_||_ "
+                + "|_||_|  | _|  |  |  | _| _|";
+
+        assertEquals("664371495 ERR", output(convertNumber(entryLine)));
+    }
+
+    /**
+     * this test is for invalid number count .
+     */
+    @Test
+    public void testBankOCRForInvalidNumberCount() {
+        String entryLine = " _  _        _        _  _ "
+                + "|_||_   |  || | _| _| _||_ "
+                + "|_||_|  |  ||_|  |  | _||_|";
+
+        assertEquals("86110??36 ILL", output(convertNumber(entryLine)));
+    }
 }
+
 
 
 
