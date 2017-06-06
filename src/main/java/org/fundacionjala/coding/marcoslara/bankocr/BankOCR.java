04a68495b6bc0329c4430ba8dd7e528102b70060
marcos-x86 <marcos.x86@outlook.com>
2017-06-06 18:07:53 -0400

    First Story.

--- src/main/java/org/fundacionjala/coding/marcoslara/bankocr/BankOCR.java
+++ src/main/java/org/fundacionjala/coding/marcoslara/bankocr/BankOCR.java
@@ -1,7 +1,44 @@
-package org.fundacionjala.coding;
+package org.fundacionjala.coding.marcoslara.bankocr;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Created by Administrator on 5/19/2017.
+ * Created by Marcos Lara on 5/19/2017.
  */
 public class BankOCR {
+
+    private static int CHAR_LONG = 3;
+    private static int LINE_LIMIT = 27;
+    private static Map<String, String> DIGITS = new HashMap<String, String>() {
+        {
+            put(" _ | ||_|", "0");
+            put("     |  |", "1");
+            put(" _  _||_ ", "2");
+            put(" _  _| _|", "3");
+            put("   |_|  |", "4");
+            put(" _ |_  _|", "5");
+            put(" _ |_ |_|", "6");
+            put(" _   |  |", "7");
+            put(" _ |_||_|", "8");
+            put(" _ |_| _|", "9");
+        }
+    };
+
+    public static String convertToNumber(String scan) {
+        StringBuilder accountNumber = new StringBuilder();
+        for (int i = 0; i < LINE_LIMIT; i += CHAR_LONG) {
+            StringBuilder number = new StringBuilder();
+            number.append(scan.substring(i, i + CHAR_LONG));
+            number.append(scan.substring(i + LINE_LIMIT, i + LINE_LIMIT + CHAR_LONG));
+            number.append(scan.substring(i + LINE_LIMIT * 2, i + LINE_LIMIT * 2 + CHAR_LONG));
+            accountNumber.append(DIGITS.get(number.toString()) == null ? "?" : DIGITS.get(number.toString()));
+        }
+        return accountNumber.toString();
+    }
+
+    public static boolean validateChecksum(String accountNumber){
+
+        return false;
+    }
 }
--- src/test/java/org/fundacionjala/coding/marcoslara/bankocr/BankOCRTest.java
+++ src/test/java/org/fundacionjala/coding/marcoslara/bankocr/BankOCRTest.java
@@ -1,7 +1,27 @@
-package org.fundacionjala.coding;
+package org.fundacionjala.coding.marcoslara.bankocr;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
- * Created by Administrator on 5/19/2017.
+ * Created by Marcos Lara on 5/19/2017.
  */
 public class BankOCRTest {
+
+    @Test
+    public void testOfTheStoryOne() {
+        String entry =  "    _  _     _  _  _  _  _ "
+                + "  | _| _||_||_ |_   ||_||_|"
+                + "  ||_  _|  | _||_|  ||_| _|";
+
+        String expectedResult = "123456789";
+        assertEquals(expectedResult, BankOCR.convertToNumber(entry));
+    }
+/*
+    @Test
+    public void validateTheCheckSumOfAKnowNumber() {
+        assertTrue(validateAccountNumber("345882865"));
+    }*/
 }
