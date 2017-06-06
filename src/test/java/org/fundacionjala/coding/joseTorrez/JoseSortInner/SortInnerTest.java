b98309fa7a8795a983310ab67b821106ce89bf8a
Jose Torrez <Jose.Torrez@fundacion-jala.org>
2017-06-06 18:05:13 -0400

    sort inner jose torrez

---
+++ src/test/java/org/fundacionjala/coding/joseTorrez/JoseSortInner/SortInnerTest.java
@@ -0,0 +1,65 @@
+package org.fundacionjala.coding.joseTorrez.JoseSortInner;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by JoseTorrez on 6/6/2017.
+ */
+public class SortInnerTest {
+    private SortInner sortInner;
+
+    /**
+     *
+     */
+    @Before
+    public void initObjects() {
+
+        sortInner = new SortInner();
+    }
+    /**
+     *
+     */
+    @Test
+    public void wordSorted() {
+
+        assertEquals("guns", sortInner.sort("guns"));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void wordSorted2() {
+
+        assertEquals("aniddg", sortInner.sort("adding"));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void wordSorted3() {
+
+        assertEquals("hlleo", sortInner.sort("hello"));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void wordSorted4() {
+
+        assertEquals("how was gonig", sortInner.innerContent("how was going"));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void wordSorted5() {
+
+        assertEquals("evrey tnihg is fnie for me", sortInner.innerContent("every thing is fine for me"));
+    }
+}
