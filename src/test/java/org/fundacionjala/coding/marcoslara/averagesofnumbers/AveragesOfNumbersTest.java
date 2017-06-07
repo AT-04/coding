27526e6d25fbc202f0fd075c1e01ad514d97875d
marcos-x86 <marcos.x86@outlook.com>
2017-06-06 20:28:56 -0400

    First commit for "Average of Numbers" Kata.

--- src/test/java/org/fundacionjala/coding/marcoslara/averagesofnumbers/AveragesOfNumbersTest.java
+++ src/test/java/org/fundacionjala/coding/marcoslara/averagesofnumbers/AveragesOfNumbersTest.java
@@ -1,7 +1,40 @@
 package org.fundacionjala.coding.marcoslara.averagesofnumbers;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
 /**
- * Created by Administrator on 6/6/2017.
+ * Created by Marcos Lara on 6/6/2017.
  */
+
 public class AveragesOfNumbersTest {
+
+    /**
+     * Basic Test for calculate de averages of array of numbers.
+     */
+    @Test
+    public void basicTests() {
+        assertEquals(Arrays.toString(new double[]{2, 2, 2, 2, 2}),
+                Arrays.toString(AveragesOfNumbers.averages(new int[]{2, 2, 2, 2, 2, 2})));
+        assertEquals(Arrays.toString(new double[]{0, 0, 0, 0}),
+                Arrays.toString(AveragesOfNumbers.averages(new int[]{2, -2, 2, -2, 2})));
+        assertEquals(Arrays.toString(new double[]{2, 4, 3, -4.5}),
+                Arrays.toString(AveragesOfNumbers.averages(new int[]{1, 3, 5, 1, -10})));
+    }
+
+    /**
+     * Basic Test for null and empty arrays of numbers.
+     */
+    @Test
+    public void nullEmptyTests() {
+        assertEquals("input 'null' should return an empty array",
+                0, AveragesOfNumbers.averages(null).length);
+        assertEquals("Empty array as input should return an empty array",
+                0, AveragesOfNumbers.averages(new int[0]).length);
+        assertEquals("Array with only one value as input should return an empty array",
+                0, AveragesOfNumbers.averages(new int[]{2}).length);
+    }
 }
