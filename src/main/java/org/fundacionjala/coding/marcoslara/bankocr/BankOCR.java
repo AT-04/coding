c7a943427a44d366d240d18a9c253303e834759c
marcos-x86 <marcos.x86@outlook.com>
2017-06-07 18:51:24 -0400

    Initial commit for new Branch for the solution of Bank OCR Kata.

--- src/main/java/org/fundacionjala/coding/marcoslara/bankocr/BankOCR.java
+++ src/main/java/org/fundacionjala/coding/marcoslara/bankocr/BankOCR.java
@@ -6,11 +6,15 @@ import java.util.Map;
 /**
  * Created by Marcos Lara on 5/19/2017.
  */
-public class BankOCR {
+public final class BankOCR {
 
-    private static int CHAR_LONG = 3;
-    private static int LINE_LIMIT = 27;
-    private static Map<String, String> DIGITS = new HashMap<String, String>() {
+    private static final int CHAR_LONG = 3;
+    private static final int LINE_LIMIT = 27;
+    private static final int CHECKSUM_DIVIDEND = 11;
+    private static final String QUESTION_MARK = "?";
+    private static final String ILLEGIBLE_MARK = " ILL";
+    private static final String ERROR_MARK = " ERR";
+    private static final Map<String, String> DIGITS = new HashMap<String, String>() {
         {
             put(" _ | ||_|", "0");
             put("     |  |", "1");
@@ -25,6 +29,19 @@ public class BankOCR {
         }
     };
 
+    /**
+     * Private constructor required. These kind of classes are more like tools/utilities than being meant for objects.
+     */
+    private BankOCR() {
+
+    }
+
+    /**
+     * This method convert the scan String to a .
+     *
+     * @param scan The input scan String.
+     * @return A account number in String format.
+     */
     public static String convertToNumber(String scan) {
         StringBuilder accountNumber = new StringBuilder();
         for (int i = 0; i < LINE_LIMIT; i += CHAR_LONG) {
@@ -37,8 +54,31 @@ public class BankOCR {
         return accountNumber.toString();
     }
 
-    public static boolean validateChecksum(String accountNumber){
+    /**
+     * This method validates de Checksum of a account number in String format.
+     *
+     * @param accountNumber A account number in String format.
+     * @return A boolean value that represents the checksum validation.
+     */
+    public static boolean validateChecksum(String accountNumber) {
+        int addition = 0;
+        for (int i = accountNumber.length() - 1; i >= 0; i--) {
+            addition += Character.getNumericValue(accountNumber.charAt(i)) * (accountNumber.length() - i);
+        }
+        return addition % CHECKSUM_DIVIDEND == 0;
+    }
 
-        return false;
+    /**
+     * This method validates that a account number is valid, illegible or invalid.
+     *
+     * @param scan The input scan String.
+     * @return A string that includes the account number and a mark in case of illegibility o invalidity.
+     */
+    public static String validateAccountNumber(String scan) {
+        String numberAccount = convertToNumber(scan);
+        if (convertToNumber(scan).contains(QUESTION_MARK)) {
+            return String.join("", numberAccount, ILLEGIBLE_MARK);
+        }
+        return validateChecksum(numberAccount) ? numberAccount : String.join("", numberAccount, ERROR_MARK);
     }
 }
