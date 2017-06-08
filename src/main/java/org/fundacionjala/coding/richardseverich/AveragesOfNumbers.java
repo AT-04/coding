3a94d9353fe2208f96a3c9c66b426d7f6c1b1a96
RichardSeverich <Richard.Severich@fundacion-jala.org>
2017-06-07 20:26:32 -0400

    Commit

--- src/main/java/org/fundacionjala/coding/richardseverich/AveragesOfNumbers.java
+++ src/main/java/org/fundacionjala/coding/richardseverich/AveragesOfNumbers.java
@@ -4,4 +4,20 @@ package org.fundacionjala.coding.richardseverich;
  * Created by Richard on 6/7/2017.
  */
 public class AveragesOfNumbers {
+
+    /**
+     * @param numbers Int Array.
+     * @return Double Array
+     */
+    public double[] averages(int[] numbers) {
+        if (numbers != null && numbers.length > 2) {
+            double[] averageNumbers;
+            averageNumbers = new double[numbers.length - 1];
+            for (int i = 0; i < numbers.length - 1; i++) {
+                averageNumbers[i] = (double) (numbers[i] + numbers[i + 1]) / 2;
+            }
+            return averageNumbers;
+        }
+        return new double[0];
+    }
 }
--- src/test/java/org/fundacionjala/coding/richardseverich/AveragesOfNumbersTest.java
+++ src/test/java/org/fundacionjala/coding/richardseverich/AveragesOfNumbersTest.java
@@ -1,7 +1,50 @@
 package org.fundacionjala.coding.richardseverich;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Richard on 6/7/2017.
  */
 public class AveragesOfNumbersTest {
+
+    private AveragesOfNumbers kata;
+
+    /**
+     *
+     */
+    @Before
+    public void setUp() {
+        kata = new AveragesOfNumbers();
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void basicTests() {
+        double[] expectedResult = new double[]{2, 2, 2, 2};
+        assertEquals(Arrays.toString(expectedResult), Arrays.toString(kata.averages(new int[]{2, 2, 2, 2, 2})));
+        expectedResult = new double[]{0, 0, 0, 0};
+        assertEquals(Arrays.toString(expectedResult), Arrays.toString(kata.averages(new int[]{2, -2, 2, -2, 2})));
+        expectedResult = new double[]{2, 4, 3, -4.5};
+        assertEquals(Arrays.toString(expectedResult), Arrays.toString(kata.averages(new int[]{1, 3, 5, 1, -10})));
+
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void nullEmptyTests() {
+        assertEquals("input 'null' should return an empty array", 0, kata.averages(null).length);
+        assertEquals("Empty array as input should return an empty array", 0, kata.averages(new int[0]).length);
+        assertEquals("Array with only one value as input should return an empty array",
+                0, kata.averages(new int[]{2}).length);
+    }
+
 }
