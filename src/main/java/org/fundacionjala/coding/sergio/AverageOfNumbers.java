06856cfca178dabd06c45e1e0bc0cc3abfff498f
Gioserden <sergio.navarro@fundacion-jala.org>
2017-06-09 14:42:15 -0400

    First try of Average Of Numbers

--- src/main/java/org/fundacionjala/coding/sergio/AverageOfNumbers.java
+++ src/main/java/org/fundacionjala/coding/sergio/AverageOfNumbers.java
@@ -3,11 +3,31 @@ package org.fundacionjala.coding.sergio;
 /**
  * Created by SergioNavarro on 6/6/2017.
  */
-public class AverageOfNumbers {
-    public static double[] averages(int[] ints) {
-        double[] prueba = new double[4];
-        for (int i = 0; i < ints.length-1; i++) {
-            prueba[i] = (ints[1]+ints[i+1])/2;
+public final class AverageOfNumbers {
+    /**
+     * By Default.
+     */
+    private AverageOfNumbers() {
+
+    }
+    /**
+     *
+     * @param ints The input array.
+     * @return The averages method.
+     */
+    static double[] averages(int[] ints) {
+        return ints == null || ints.length <= 1 ? new double[0] : getDoubles(ints);
+    }
+
+    /**
+     *
+     * @param ints same input array.
+     * @return operation.
+     */
+    private static double[] getDoubles(int[] ints) {
+        double[] prueba = new double[ints.length - 1];
+        for (int i = 0; i < ints.length - 1; i++) {
+            prueba[i] = (double) (ints[i] + ints[i + 1]) / 2;
         }
         return prueba;
     }
--- src/test/java/org/fundacionjala/coding/sergio/AverageOfNumbersTest.java
+++ src/test/java/org/fundacionjala/coding/sergio/AverageOfNumbersTest.java
@@ -10,12 +10,28 @@ import static junit.framework.TestCase.assertEquals;
  * Created by SergioNavarro on 6/6/2017.
  */
 public class AverageOfNumbersTest {
-
+    /**
+     *
+     */
     @Test
-    public void basicTest() {
+    public void basicTest1() {
 
-        assertEquals(Arrays.toString(new double[] { 2, 2, 2, 2 }), Arrays.toString(AverageOfNumbers.averages(new int[] { 2, 2, 2, 2, 2 })));
+        assertEquals(Arrays.toString(new double[] {2, 2, 2, 2}),
+                Arrays.toString(AverageOfNumbers.averages(new int[] {2, 2, 2, 2, 2})));
+        assertEquals(Arrays.toString(new double[] {0, 0, 0, 0}),
+                Arrays.toString(AverageOfNumbers.averages(new int[] {2, -2, 2, -2, 2})));
+        assertEquals(Arrays.toString(new double[] {2, 4, 3, -4.5}),
+                Arrays.toString(AverageOfNumbers.averages(new int[] {1, 3, 5, 1, -10})));
+    }
 
+    /**
+     *
+     */
+    @Test
+    public void nullEmptyTests() {
+        assertEquals(0, AverageOfNumbers.averages(null).length);
+        assertEquals(0, AverageOfNumbers.averages(new int[0]).length);
+        assertEquals(0, AverageOfNumbers.averages(new int[] {2}).length);
     }
 
 }
