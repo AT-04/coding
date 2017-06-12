29f8407ef206ef4c367af6c79c553023928aaf52
John Doe <yveryury@gmail.com>
2017-06-12 11:06:21 -0400

    final exercise of kata

--- src/main/java/org/fundacionjala/coding/yury/AveragesOfNumbers.java
+++ src/main/java/org/fundacionjala/coding/yury/AveragesOfNumbers.java
@@ -3,20 +3,22 @@ package org.fundacionjala.coding.yury;
 /**
  * Created by YuryOrtuno on 6/6/2017.
  */
-public class AveragesOfNumbers {
+public final class AveragesOfNumbers {
+    /**
+     * Constructor.
+     */
+    private AveragesOfNumbers() {
+    }
 
     /**
      * @param ints String de numbers.
      * @return solve
      */
-    public double[] averages(int[] ints) {
+    public static double[] averages(int[] ints) {
         int sizeArray = (ints == null || ints.length <= 1) ? 0 : ints.length - 1;
         double[] averagesArray = (sizeArray == 0) ? new double[0] : new double[sizeArray];
-        int i = 0;
-        while (sizeArray > 0) {
+        for (int i = 0; i < sizeArray; i++) {
             averagesArray[i] = (double) (ints[i] + ints[i + 1]) / 2;
-            i++;
-            sizeArray--;
         }
         return averagesArray;
     }
