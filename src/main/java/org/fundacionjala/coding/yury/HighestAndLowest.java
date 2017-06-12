cd1b07bf7e19a9b3ed1f0d529ec3b413081d7fcd
John Doe <yveryury@gmail.com>
2017-06-12 18:56:51 -0400

    initial solution

--- src/main/java/org/fundacionjala/coding/yury/HighestAndLowest.java
+++ src/main/java/org/fundacionjala/coding/yury/HighestAndLowest.java
@@ -1,7 +1,26 @@
 package org.fundacionjala.coding.yury;
 
+
+import java.util.Collections;
+import java.util.LinkedList;
+
 /**
  * Created by YuryOrtuno on 6/12/2017.
  */
 public class HighestAndLowest {
+
+    /**
+     * @param string
+     * @return
+     */
+    public String highAndLow(String string) {
+
+        LinkedList<Integer> list = new LinkedList<>();
+        String[] listNumber = string.split(" ");
+        for (String number : listNumber) {
+            list.add(Integer.parseInt(number));
+        }
+        Collections.sort(list);
+        return String.format("%d %d", list.getLast(), list.getFirst());
+    }
 }
--- src/test/java/org/fundacionjala/coding/yury/HighestAndLowestTests.java
+++ src/test/java/org/fundacionjala/coding/yury/HighestAndLowestTests.java
@@ -1,7 +1,62 @@
 package org.fundacionjala.coding.yury;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by YuryOrtuno on 6/12/2017.
  */
 public class HighestAndLowestTests {
+
+    @Test
+    public void SomeTest() {
+        HighestAndLowest highestAndLowest=new HighestAndLowest();
+        assertThat(highestAndLowest.highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"), is("542 -214"));
+    }
+
+
+
+    @Test
+    public void PlusMinusTest() {
+        HighestAndLowest highestAndLowest=new HighestAndLowest();
+        assertThat(highestAndLowest.highAndLow("1 -1"), is("1 -1"));
+    }
+
+    @Test
+    public void PlusPlusTest() {
+        HighestAndLowest highestAndLowest=new HighestAndLowest();
+        assertThat(highestAndLowest.highAndLow("1 1"), is("1 1"));
+    }
+
+    @Test
+    public void MinusMinusTest() {
+        HighestAndLowest highestAndLowest=new HighestAndLowest();
+        assertThat(highestAndLowest.highAndLow("-1 -1"), is("-1 -1"));
+    }
+
+    @Test
+    public void PlusMinusZeroTest() {
+        HighestAndLowest highestAndLowest=new HighestAndLowest();
+        assertThat(highestAndLowest.highAndLow("1 -1 0"), is("1 -1"));
+    }
+
+    @Test
+    public void PlusPlusZeroTest() {
+        HighestAndLowest highestAndLowest=new HighestAndLowest();
+        assertThat(highestAndLowest.highAndLow("1 1 0"), is("1 0"));
+    }
+
+    @Test
+    public void MinusMinusZeroTest() {
+        HighestAndLowest highestAndLowest=new HighestAndLowest();
+        assertThat(highestAndLowest.highAndLow("-1 -1 0"), is("0 -1"));
+    }
+
+    @Test
+    public void SingleTest() {
+        HighestAndLowest highestAndLowest=new HighestAndLowest();
+        assertThat(highestAndLowest.highAndLow("42"), is("42 42"));
+    }
 }
