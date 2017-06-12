1391ce38422bfd6f1884449a68d3ad7d1ffa4b53
RichardSeverich <richard.severich@fundacion-jala.org>
2017-06-12 19:12:10 -0400

    Richard Severich

--- src/main/java/org/fundacionjala/coding/richardseverich/HighLow.java
+++ src/main/java/org/fundacionjala/coding/richardseverich/HighLow.java
@@ -5,7 +5,24 @@ package org.fundacionjala.coding.richardseverich;
  */
 public class HighLow {
 
-    public String HighLow(){
+    /**
+     * @param cad is a string.
+     * @return String high and low.
+     */
+    public String highAndLow(String cad) {
 
+        String[] arrayNum = cad.split(" ");
+        int high = Integer.parseInt(arrayNum[0]);
+        int low = Integer.parseInt(arrayNum[0]);
+
+        for (int i = 0; i < arrayNum.length; i++) {
+            if (Integer.parseInt(arrayNum[i]) > high) {
+                high = Integer.parseInt(arrayNum[i]);
+            }
+            if (low > Integer.parseInt(arrayNum[i])) {
+                low = Integer.parseInt(arrayNum[i]);
+            }
+        }
+        return String.format("%d %d", high, low);
     }
 }
--- src/test/java/org/fundacionjala/coding/richardseverich/MultiplesTest.java
+++ src/test/java/org/fundacionjala/coding/richardseverich/MultiplesTest.java
@@ -1,7 +1,22 @@
 package org.fundacionjala.coding.richardseverich;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by RichardSeverich on 6/12/2017.
  */
 public class MultiplesTest {
+
+    /**
+     *
+     */
+    @Test
+    public void test() {
+        Multiples kata = new Multiples();
+        assertEquals(23, kata.solution(10));
+        assertEquals(78, kata.solution(20));
+        assertEquals(9168, kata.solution(200));
+    }
 }
