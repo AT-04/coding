20a8d5c6cbcc4ec8f56e0a9c53b87c5c96fe2d11
Ovidio Miranda <Ovidio.Miranda@fundacion-jala.org>
2017-06-12 19:43:41 -0400

    Final commit HighestAndLowest.

--- src/main/java/org/fundacionjala/coding/ovidio/HighestAndLowest.java
+++ src/main/java/org/fundacionjala/coding/ovidio/HighestAndLowest.java
@@ -1,7 +1,32 @@
 package org.fundacionjala.coding.ovidio;
 
+import java.util.Arrays;
+
 /**
  * Created by OvidioMiranda on 6/12/2017.
  */
-public class HighestAndLowest {
+
+public final class HighestAndLowest {
+    /**
+     *
+     */
+    private HighestAndLowest() {
+    }
+
+    /**
+     * @param sentence is String.
+     * @return new String.
+     */
+    public static String highAndLow(String sentence) {
+        String[] numbersArray = sentence.split(" ");
+        int[] arrayListInt = new int[numbersArray.length];
+        int i = 0;
+        for (String number : numbersArray) {
+            arrayListInt[i] = Integer.parseInt(number);
+            i++;
+        }
+        Arrays.sort(arrayListInt);
+        return String.format("%d %d", arrayListInt[arrayListInt.length - 1], arrayListInt[0]);
+    }
+
 }
