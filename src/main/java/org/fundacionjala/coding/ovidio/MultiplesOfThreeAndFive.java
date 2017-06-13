2fe4c9407f7db061a60cb6d36f1df1d6051a7dad
Ovidio Miranda <Ovidio.Miranda@fundacion-jala.org>
2017-06-13 10:37:30 -0400

    Correcting.

--- src/main/java/org/fundacionjala/coding/ovidio/MultiplesOfThreeAndFive.java
+++ src/main/java/org/fundacionjala/coding/ovidio/MultiplesOfThreeAndFive.java
@@ -11,21 +11,16 @@ public final class MultiplesOfThreeAndFive {
     }
 
     /**
-     *
      * @param max number.
      * @return number.
      */
     public static int solution(int max) {
-        int  sum=0, i=0;
-        while(i<max)
-        {
+        int sum = 0, i = 0;
+        while (i < max) {
             sum += (i % 3 == 0 || i % 5 == 0) ? i : 0;
             i++;
         }
         return sum;
     }
-
-
-
 
 }
