4ff15b4b435ffb01f1132d407e87eb69a4f2a091
Ovidio Miranda <Ovidio.Miranda@fundacion-jala.org>
2017-06-12 20:44:50 -0400

    Second Commit.

--- src/main/java/org/fundacionjala/coding/ovidio/MultiplesOfThreeAndFive.java
+++ src/main/java/org/fundacionjala/coding/ovidio/MultiplesOfThreeAndFive.java
@@ -3,5 +3,26 @@ package org.fundacionjala.coding.ovidio;
 /**
  * Created by OvidioMiranda on 6/12/2017.
  */
-public class MultiplesOfThreeAndFive {
+public final class MultiplesOfThreeAndFive {
+    /**
+     *
+     */
+    private MultiplesOfThreeAndFive() {
+    }
+
+    /**
+     *
+     * @param number adasd.
+     * @return asdas.
+     */
+    public static int solution(int number) {
+        int result = 0;
+        for (int i = 0; i < number; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                result += i;
+            }
+        }
+        return result;
+    }
+
 }
