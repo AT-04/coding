84a42b0955b11f4ae1b258a94855f61f534d2b48
Ovidio Miranda <Ovidio.Miranda@fundacion-jala.org>
2017-06-13 19:09:11 -0400

    Second Commit.

--- src/main/java/org/fundacionjala/coding/ovidio/Evaporator.java
+++ src/main/java/org/fundacionjala/coding/ovidio/Evaporator.java
@@ -3,5 +3,58 @@ package org.fundacionjala.coding.ovidio;
 /**
  * Created by OvidioMiranda on 6/13/2017.
  */
-public class Evaporator {
+public final class Evaporator {
+
+    /**
+     *
+     */
+    private Evaporator() {
+    }
+
+    /**
+     * @param content    content.
+     * @param evapPerDay evap per day.
+     * @param threshold  threshold.
+     * @return int
+     */
+    public static int evaporator(double content, double evapPerDay, double threshold) {
+        int result = 0;
+        double percent = 100;
+        while (percent > threshold) {
+            percent -= percent * (evapPerDay / 100);
+            result++;
+        }
+        return result;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    public static int evaporator(double content, double evap_per_day, double threshold) {
+
+        int result = 0;
+        double percent = 100;
+        while (percent > threshold)
+        {
+            percent = percent - percent * (evap_per_day/100);
+            result++;
+        }
+        return result;
+
+    }
+    */
+
