8cd94ecd1a9fc38bd3eb0e40e827bb6c3a47563f
John Doe <yveryury@gmail.com>
2017-06-13 18:49:09 -0400

    Finish Exam Coding

--- src/main/java/org/fundacionjala/coding/yury/Exam.java
+++ src/main/java/org/fundacionjala/coding/yury/Exam.java
@@ -1,7 +1,33 @@
 package org.fundacionjala.coding.yury;
 
+
 /**
  * Created by YuryOrtuno on 6/13/2017.
  */
-public class Exam {
+public final class Exam {
+
+    private static final int PORCENTAJE = 100;
+
+    /**
+     * Constructor.
+     */
+    private Exam() {
+    }
+
+    /**
+     * @param content      of the evaporator.
+     * @param evapPerDay gas lost every day.
+     * @param umbral       percentage beyond which the evaporator is no longer useful.
+     * @return day the evaporator will be out of use.
+     */
+    public static int evaporator(double content, double evapPerDay, double umbral) {
+
+        int i = 0;
+        double min = content * umbral / PORCENTAJE;
+        while (content >= min) {
+            content -= content * (evapPerDay / PORCENTAJE);
+            i++;
+        }
+        return i;
+    }
 }
