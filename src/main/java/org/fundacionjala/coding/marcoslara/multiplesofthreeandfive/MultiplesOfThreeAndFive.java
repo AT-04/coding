707c912fb9f54677f58f7ba95b70c0672b6e2909
marcos-x86 <marcos.x86@outlook.com>
2017-06-12 22:36:19 -0400

    Coding Kata - Multiples of three and five Initial commit.

--- src/main/java/org/fundacionjala/coding/marcoslara/multiplesofthreeandfive/MultiplesOfThreeAndFive.java
+++ src/main/java/org/fundacionjala/coding/marcoslara/multiplesofthreeandfive/MultiplesOfThreeAndFive.java
@@ -3,5 +3,26 @@ package org.fundacionjala.coding.marcoslara.multiplesofthreeandfive;
 /**
  * Created by Marcos on 12-06-2017.
  */
-public class MultiplesOfThreeAndFive {
+public final class MultiplesOfThreeAndFive {
+
+    /**
+     * Private constructor required. These kind of classes are more like tools/utilities than being meant for objects.
+     */
+    private MultiplesOfThreeAndFive() {
+
+    }
+
+    /**
+     * Main method that calculates the solution of the sum of the multiples of three and five.
+     *
+     * @param number Is the parameter number from which the solution is to be calculated.
+     * @return The sum of the multiples of three and five.
+     */
+    public static int solution(int number) {
+        int sum = 0;
+        for (int i = 3; i <= (number - number % 3); i++) {
+            sum += (i % 3 == 0 || i % 5 == 0) ? i : 0;
+        }
+        return sum;
+    }
 }
