9e9100bf1cd2a836ca554a269cb863f0bc6b33b3
John Doe <yveryury@gmail.com>
2017-06-12 20:18:14 -0400

    solution multiple 3 and 5

--- src/main/java/org/fundacionjala/coding/yury/Multiple.java
+++ src/main/java/org/fundacionjala/coding/yury/Multiple.java
@@ -3,5 +3,28 @@ package org.fundacionjala.coding.yury;
 /**
  * Created by YuryOrtuno on 6/12/2017.
  */
-public class Multiple {
+public final class Multiple {
+    private static final int THREE = 3;
+    private static final int FIVE = 5;
+
+    /**
+     * Constructor.
+     */
+    private Multiple() {
+    }
+
+    /**
+     * @param max number
+     * @return sum multiples 3 and 5
+     */
+    public static int solution(int max) {
+
+        int sum = 0;
+        for (int i = 1; i < max; i++) {
+            if (i % THREE == 0 || i % FIVE == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
 }
