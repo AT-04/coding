e6a6a3937a9f5f07b4db57edb1aeae6ee4582683
marcos-x86 <marcos.x86@outlook.com>
2017-06-12 21:56:08 -0400

    Coding Kata - Highest and Lowest Initial commit.

--- src/main/java/org/fundacionjala/coding/marcoslara/highestandlowest/HighestAndLowest.java
+++ src/main/java/org/fundacionjala/coding/marcoslara/highestandlowest/HighestAndLowest.java
@@ -1,7 +1,29 @@
 package org.fundacionjala.coding.marcoslara.highestandlowest;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Created by Marcos on 12-06-2017.
  */
-public class HighestAndLowest {
+public final class HighestAndLowest {
+    /**
+     * Private constructor required. These kind of classes are more like tools/utilities than being meant for objects.
+     */
+    private HighestAndLowest() {
+
+    }
+
+    /**
+     * Main method that find the highest and lowest values of a numbers group.
+     *
+     * @param input String with the group of numbers.
+     * @return A string with the highest and lowest values.
+     */
+    public static String highAndLow(String input) {
+        List<Integer> numbers = Arrays.stream(input.split(" ")).map(Integer::valueOf).collect(Collectors.toList());
+        return String.join(" ", Collections.max(numbers).toString(), Collections.min(numbers).toString());
+    }
 }
