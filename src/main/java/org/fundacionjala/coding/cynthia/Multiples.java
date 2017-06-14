074a7611b980b6ba263314ea6767553d1229aa8c
Cynthia Terrazas <cynthia.terrazas@fundacion-jala.org>
2017-06-14 12:23:48 -0400

    multiples

--- src/main/java/org/fundacionjala/coding/cynthia/Multiples.java
+++ src/main/java/org/fundacionjala/coding/cynthia/Multiples.java
@@ -3,5 +3,24 @@ package org.fundacionjala.coding.cynthia;
 /**
  * Created by CynthiaTerrazas on 6/14/2017.
  */
-public class Multiples {
+public final class Multiples {
+    /**
+     * this is the constructor.
+     */
+    private Multiples() {
+    }
+
+    /**
+     * @param limite numero limite.
+     * @return suma de los multiplos.
+     */
+    public static int solution(int limite) {
+        int suma = 0;
+        for (int i = 1; i < limite; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                suma += i;
+            }
+        }
+        return suma;
+    }
 }
--- src/test/java/org/fundacionjala/coding/cynthia/MultiplesTest.java
+++ src/test/java/org/fundacionjala/coding/cynthia/MultiplesTest.java
@@ -1,7 +1,36 @@
 package org.fundacionjala.coding.cynthia;
 
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 /**
  * Created by CynthiaTerrazas on 6/14/2017.
  */
 public class MultiplesTest {
+    /**
+     *
+     */
+    @Test
+    public void theFirstTest() {
+        assertEquals(23, Multiples.solution(10));
+    }
+
+
+    /**
+     *
+     */
+    @Test
+    public void theSecondtest() {
+        assertEquals(78, Multiples.solution(20));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void thelastTest() {
+        assertEquals(9168, Multiples.solution(200));
+    }
+
 }
