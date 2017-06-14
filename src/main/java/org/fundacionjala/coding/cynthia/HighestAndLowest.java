51a4273e36b10b87f3ec6ad227ddaa9e728ffd62
cynthia terrazas gutierrez <star_ok@hotmail.com>
2017-06-13 23:56:43 -0400

    solution HighestAndLowestTest

---
+++ src/main/java/org/fundacionjala/coding/cynthia/HighestAndLowest.java
@@ -0,0 +1,33 @@
+package org.fundacionjala.coding.cynthia;
+
+/**
+ * Created by CynthiaTerrazas on 6/12/2017.
+ */
+public final class HighestAndLowest {
+
+    /**
+     * constructor.
+     */
+    private HighestAndLowest() {
+    }
+
+    /**
+     * @param cadena number.
+     * @return calculate value.
+     */
+    public static String calculated(String cadena) {
+        String[] setNumbers = cadena.split(" ");
+        int mayor = -100;
+        int menor = 100;
+        for (String word : setNumbers) {
+            int numero = Integer.parseInt(word);
+            if (numero >= mayor) {
+                mayor = numero;
+            }
+            if (numero <= menor) {
+                menor = numero;
+            }
+        }
+        return String.format("%d %d", mayor, menor);
+    }
+}
