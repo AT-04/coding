40eaf1e1a67e4cc8a0b0b0e45deda0f8d767f852
Gioserden <sergio.navarro@fundacion-jala.org>
2017-06-14 19:58:53 -0400

    Sergio bank ocr (#6)
    * Initial solution for Back OCR kata
    
    * Initial solution for Back OCR
    
    * finish first try
    
    * Rename bank ocr classes
    
    * test
    
    * First refactor
    
    * Add a unit Test
    
    * Adding story 2 to code.
    
    * User Story 3 first Try
    
    * first Refactor
    
    * Correct Test Title.
    
    * Second Refac Bank
    
    * third time final refac

---
+++ src/main/java/org/fundacionjala/coding/sergio/BankOCR.java
@@ -0,0 +1,105 @@
+package org.fundacionjala.coding.sergio;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 5/16/2017.
+ */
+final class BankOCR {
+    private static final int THREE = 3;
+    private static final int TWO = 2;
+    private static final int NINE = 9;
+    private static final int FOUR = 4;
+    private static final int FIVE = 5;
+    private static final int SIX = 6;
+    private static final int SEVEN = 7;
+    private static final int EIGHT = 8;
+    private static final int MAXLENGTH = 27;
+    private static final int ZEROCONDITION = 0;
+    private static final int CHECKSUMCONDITION = 11;
+    private static final int ONE = 1;
+    private static final String S_ERR = "%s ERR";
+    private static final String S_ILL = "%s ILL";
+    private static final String NON_VALID_NUMBER = "?";
+    private static final Map<String, Integer> NUMBER;
+
+    static {
+        NUMBER = new HashMap<>();
+        NUMBER.put("   "
+                + "  |"
+                + "  |", ONE);
+        NUMBER.put(" _ "
+                + " _|"
+                + "|_ ", TWO);
+        NUMBER.put(" _ "
+                + " _|"
+                + " _|", THREE);
+        NUMBER.put("   "
+                + "|_|"
+                + "  |", FOUR);
+        NUMBER.put(" _ "
+                + "|_ "
+                + " _|", FIVE);
+        NUMBER.put(" _ "
+                + "|_ "
+                + "|_|", SIX);
+        NUMBER.put(" _ "
+                + "  |"
+                + "  |", SEVEN);
+        NUMBER.put(" _ "
+                + "|_|"
+                + "|_|", EIGHT);
+        NUMBER.put(" _ "
+                + "|_|"
+                + " _|", NINE);
+        NUMBER.put(" _ "
+                + "| |"
+                + "|_|", 0);
+    }
+
+    /**
+     * CONSTRUCTOR.
+     */
+    private BankOCR() {
+    }
+
+    /**
+     * @param line1 IS THE FIRST LINE OF ENTRY.
+     * @param line2 IS THE SECOND LINE OF ENTRY.
+     * @param line3 IS THE THIRD LINE OF ENTRY.
+     * @return THE NUMBER OF MAP IND.
+     */
+    static String convertEntryToNumber(String line1, String line2, String line3) {
+        StringBuilder finalString = new StringBuilder();
+        for (int i = 0; i < MAXLENGTH; i = i + THREE) {
+            int j = i + THREE;
+            finalString.append(NUMBER.get(line1.substring(i, j) + line2.substring(i, j) + line3.substring(i, j)));
+        }
+        return finalString.toString();
+    }
+
+    /**
+     * @param number is the a/c #.
+     * @return if the a/c is valid or not.
+     */
+    static boolean validationCheckSum(String number) {
+        String[] num = new StringBuilder(number).reverse().toString().split("");
+        int checkSum = 0;
+        for (int i = 0; i < number.length(); i++) {
+            checkSum += (i + ONE) * Integer.parseInt(num[i]);
+        }
+        return checkSum % CHECKSUMCONDITION == ZEROCONDITION;
+    }
+
+    /**
+     * @param entry String entry.
+     * @return The comparation.
+     */
+    static String errorAccount(String entry) {
+        if (entry.contains(NON_VALID_NUMBER)) {
+            return String.format(S_ILL, entry);
+        }
+        return !validationCheckSum(entry) ? String.format(S_ERR, entry) : entry;
+    }
+}
---
+++ src/test/java/org/fundacionjala/coding/sergio/BankOCRTest.java
@@ -0,0 +1,75 @@
+package org.fundacionjala.coding.sergio;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Created by Administrator on 5/16/2017.
+ */
+public class BankOCRTest {
+    /**
+     *
+     */
+    @Test
+    public void comprobateEntryNumbersOneToNine() {
+
+        String firstLine = "    _  _     _  _  _  _  _ ";
+        String secndLine = "  | _| _||_||_ |_   ||_||_|";
+        String thirdLine = "  ||_  _|  | _||_|  ||_| _|";
+        assertEquals("123456789", BankOCR.convertEntryToNumber(firstLine, secndLine, thirdLine));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void checkEntryOnlyZeros() {
+        String firstLine = " _  _  _  _  _  _  _  _  _ ";
+        String secndLine = "| || || || || || || || || |";
+        String thirdLine = "|_||_||_||_||_||_||_||_||_|";
+        Assert.assertEquals("000000000", BankOCR.convertEntryToNumber(firstLine, secndLine, thirdLine));
+    }
+    /**
+     *
+     */
+    @Test
+    public void checkSumValidationTrue() {
+        String entry = "345882865";
+
+        assertTrue(BankOCR.validationCheckSum(entry));
+    }
+    /**
+     *
+     */
+    @Test
+    public void checkSumValidationFalse() {
+        String entry = "356987453";
+
+        assertFalse(BankOCR.validationCheckSum(entry));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void userStoryThreeCaseERR() {
+        String entry = "356987453";
+
+        assertEquals("356987453 ERR", BankOCR.errorAccount(entry));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void userStoryThreeCaseILL() {
+        String entry = "86110??36";
+
+        assertEquals("86110??36 ILL", BankOCR.errorAccount(entry));
+    }
+}
