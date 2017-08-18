9a52f3477c1f868f78f4f7aac2b738244a6dfe4e
Ovidio Miranda <Ovidio.Miranda@fundacion-jala.org>
2017-08-18 19:29:37 -0400

    Removing spaces

--- src/main/java/org/fundacionjala/coding/ovidio/Sequence.java
+++ src/main/java/org/fundacionjala/coding/ovidio/Sequence.java
@@ -3,21 +3,15 @@ package org.fundacionjala.coding.ovidio;
 /**
  * Created by OvidioMiranda on 8/18/2017.
  */
-public final class Sequence {
+public class Sequence {
 
-    /**
-     *
-     */
-    private Sequence() {
-    }
-
-    /**
+     /**
      * Get Score.
      *
      * @param n Long.
      * @return Long
      */
-    public static long getScore(final long n) {
+    public  long getScore(final long n) {
         return 25 * n * (n + 1);
     }
 }
