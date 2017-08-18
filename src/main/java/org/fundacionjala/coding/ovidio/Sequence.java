7cc59ee6f666c8ccb3ffb84687ece55e2bac4745
Ovidio Miranda <Ovidio.Miranda@fundacion-jala.org>
2017-08-18 17:56:03 -0400

    The solution for the secuence kata

---
+++ src/main/java/org/fundacionjala/coding/ovidio/Sequence.java
@@ -0,0 +1,23 @@
+package org.fundacionjala.coding.ovidio;
+
+/**
+ * Created by OvidioMiranda on 8/18/2017.
+ */
+public final class Sequence {
+
+    /**
+     *
+     */
+    private Sequence() {
+    }
+
+    /**
+     * Get Score.
+     *
+     * @param n Long.
+     * @return Long
+     */
+    public static long getScore(final long n) {
+        return 25 * n * (n   + 1);
+    }
+}
