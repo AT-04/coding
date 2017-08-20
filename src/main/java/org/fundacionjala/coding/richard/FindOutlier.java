ef3626d22d2d66bce9a711b604dbb1827e7cbba4
RichardSeverich <Richard.Severich@fundacion-jala.org>
2017-08-19 23:28:58 -0400

    Fix Coverage

--- src/main/java/org/fundacionjala/coding/richard/FindOutlier.java
+++ src/main/java/org/fundacionjala/coding/richard/FindOutlier.java
@@ -12,30 +12,30 @@ package org.fundacionjala.coding.richard;
  * [160, 3, 1719, 19, 11, 13, -21]
  * Should return: 160
  */
-public final class FindOutlier {
+public class FindOutlier {
 
     /**
      * This method is a private constructor.
      */
-    private FindOutlier() {
+    public FindOutlier() {
     }
 
     /**
      * @param outlier is a array of int numbers.
      * @return outlier number.
      */
-    public static int find(int[] outlier) {
+    int find(int[] outlier) {
         int pairResult = 0;
         int oddResult = 0;
         int countPairNumbers = 0;
         int countOddNumbers = 0;
-        for (int i = 0; i < outlier.length; i++) {
-            if (outlier[i] % 2 == 0) {
+        for (int anOutlier : outlier) {
+            if (anOutlier % 2 == 0) {
                 countPairNumbers++;
-                pairResult = outlier[i];
+                pairResult = anOutlier;
             } else {
                 countOddNumbers++;
-                oddResult = outlier[i];
+                oddResult = anOutlier;
             }
         }
         return (countPairNumbers > countOddNumbers) ? oddResult : pairResult;
