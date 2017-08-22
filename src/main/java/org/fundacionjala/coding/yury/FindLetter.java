a91f3febdc78c4667d636b2ba6e20defe0f3cb56
yuryver <yury.ortuno@fundacion-jala.org>
2017-08-22 13:35:33 -0400

    corrections

--- src/main/java/org/fundacionjala/coding/yury/FindLetter.java
+++ src/main/java/org/fundacionjala/coding/yury/FindLetter.java
@@ -1,10 +1,16 @@
 package org.fundacionjala.coding.yury;
 
 /**
- * Created by yuryy on 22/8/2017.
+ * This method find a missing letter.
  */
 public class FindLetter {
-  public static char findMissingLetter(char[] array) {
+  /**
+   * This method find a missing letter.
+   *
+   * @param array with characters.
+   * @return missing letter.
+   */
+  public char findMissingLetter(char[] array) {
     for (int i = 1; i < array.length; i++) {
       if (array[i] != array[i - 1] + 1) {
         return (char) (array[i] - 1);
--- src/test/java/org/fundacionjala/coding/yury/FindLetterTest.java
+++ src/test/java/org/fundacionjala/coding/yury/FindLetterTest.java
@@ -1,13 +1,32 @@
 package org.fundacionjala.coding.yury;
 
+import org.junit.Before;
 import org.junit.Test;
-    import static org.junit.Assert.assertEquals;
-    import org.junit.runners.JUnit4;
 
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test.
+ */
 public class FindLetterTest {
+
+  private FindLetter findLetter;
+
+  /**
+   * This Before.
+   */
+  @Before
+  public void setUp() {
+    findLetter = new FindLetter();
+  }
+
+  /**
+   * Example Test.
+   */
   @Test
   public void exampleTests() {
-    assertEquals('e', FindLetter.findMissingLetter(new char[] { 'a','b','c','d','f' }));
-    assertEquals('P', FindLetter.findMissingLetter(new char[] { 'O','Q','R','S' }));
+    assertEquals('e', findLetter.findMissingLetter(new char[]{'a', 'b', 'c', 'd', 'f'}));
+    assertEquals('P', findLetter.findMissingLetter(new char[]{'O', 'Q', 'R', 'S'}));
+    assertEquals(' ', findLetter.findMissingLetter(new char[]{'P', 'Q', 'R', 'S'}));
   }
 }
