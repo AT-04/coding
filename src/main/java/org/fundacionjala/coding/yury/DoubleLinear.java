22b6a76aa32ee7a144aba866794164c1f4d24f83
yuryver <yury.ortuno@fundacion-jala.org>
2017-08-23 17:27:03 -0400

    solve v1

--- src/main/java/org/fundacionjala/coding/yury/DoubleLinear.java
+++ src/main/java/org/fundacionjala/coding/yury/DoubleLinear.java
@@ -9,38 +9,38 @@ import java.util.Collections;
 public class DoubleLinear {
 
 
-  private static int sequence(int n) {
+    private static int sequence(int n) {
 
-    ArrayList list = new ArrayList();
-    int initial = 1;
-    int val = 0;
-    int value = 1;
-    list.add(1);
-    for (int i = 0; i <= n; i++) {
-      if (!list.contains(equationY((Integer) list.get(i)))) {
-        list.add(equationY((Integer) list.get(i)));
-      }
-      if (!list.contains(equationZ((Integer) list.get(i)))) {
-        list.add(equationZ((Integer) list.get(i)));
-      }
-
-      Collections.sort(list);
+        ArrayList list = new ArrayList();
+        list.add(1);
+        int i = 0;
+        while (list.size() < 1.5 * n) {
+            if (!list.contains(equationY((Integer) list.get(i)))) {
+                list.add(equationY((Integer) list.get(i)));
+            }
+            if (!list.contains(equationZ((Integer) list.get(i)))) {
+                list.add(equationZ((Integer) list.get(i)));
+            }
+            Collections.sort(list);
+            i++;
+        }
+        return (int) list.get(n);
     }
-    System.out.println(list);
-    return (int) list.get(n);
-  }
 
-  private static int equationY(int value) {
-    return 2 * value + 1;
-  }
+    private static boolean condition(ArrayList list, int i, int n) {
+        return list.size() >= n && equationY((Integer) list.get(i)) >= (int) list.get(n);
+    }
 
-  private static int equationZ(int value) {
-    return 3 * value + 1;
-  }
+    private static int equationY(int value) {
+        return 2 * value + 1;
+    }
 
-  public static int dblLinear(int i) {
-    return sequence(i);
-  }
+    private static int equationZ(int value) {
+        return 3 * value + 1;
+    }
 
+    static int dblLinear(int i) {
+        return sequence(i);
+    }
 
 }
--- src/test/java/org/fundacionjala/coding/yury/DoubleLinearTest.java
+++ src/test/java/org/fundacionjala/coding/yury/DoubleLinearTest.java
@@ -14,9 +14,10 @@ public class DoubleLinearTest {
   public void test() {
     System.out.println("Fixed Tests dblLinear");
     testing(DoubleLinear.dblLinear(10), 22);
-//    testing(DoubleLinear.dblLinear(20), 57);
-//    testing(DoubleLinear.dblLinear(30), 91);
-//    testing(DoubleLinear.dblLinear(50), 175);
+
+    testing(DoubleLinear.dblLinear(20), 57);
+    testing(DoubleLinear.dblLinear(30), 91);
+    testing(DoubleLinear.dblLinear(50), 175);
 
   }
 }
