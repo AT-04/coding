cf2bd144a1c2742db409ae0786369481a75a458c
Abel Barrientos <abel.barrientos@fundacion-jala.org>
2017-08-23 08:53:52 -0400

    Solving the kata

---
+++ src/test/java/org/fundacionjala/coding/abel/TenMinuteWalkTest.java
@@ -0,0 +1,49 @@
+package org.fundacionjala.coding.abel;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * This class test the Ten Minute Walk class.
+ */
+public class TenMinuteWalkTest {
+
+    /**
+     * Test for Private Constructors.
+     *
+     * @throws NoSuchMethodException     Thrown when a particular method cannot be found.
+     * @throws IllegalAccessException    Thrown when an application tries to reflectively create an instance.
+     * @throws InvocationTargetException thrown by an invoked method or constructor.
+     * @throws InstantiationException    Thrown when an application tries to create an instance of a class
+     *                                   using the {@code newInstance} method in class.
+     */
+    @Test
+    public void privateConstructorTest() throws NoSuchMethodException, IllegalAccessException,
+            InvocationTargetException, InstantiationException {
+        Constructor<TenMinWalk> constructor = TenMinWalk.class.getDeclaredConstructor();
+        assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+        constructor.setAccessible(true);
+        constructor.newInstance();
+    }
+
+    /**
+     * All provided tests.
+     */
+    @Test
+    public void test() {
+        assertEquals("Should return true", true,
+                TenMinWalk.isValid(new char[]{'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'}));
+        assertEquals("Should return false", false,
+                TenMinWalk.isValid(new char[]{'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e'}));
+        assertEquals("Should return false", false,
+                TenMinWalk.isValid(new char[]{'w'}));
+        assertEquals("Should return false", false,
+                TenMinWalk.isValid(new char[]{'n', 'n', 'n', 's', 'n', 's', 'n', 's', 'n', 's'}));
+    }
+}
