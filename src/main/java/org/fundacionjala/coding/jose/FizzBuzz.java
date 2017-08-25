eb3d8e48c501f158f32049ef4d80354dc305ddd1
Jose Torrez <Jose.Torrez@fundacion-jala.org>
2017-08-25 18:45:27 -0400

    FizzBuzz solution try

--- src/main/java/org/fundacionjala/coding/jose/FizzBuzz.java
+++ src/main/java/org/fundacionjala/coding/jose/FizzBuzz.java
@@ -3,5 +3,32 @@ package org.fundacionjala.coding.jose;
 /**
  * Created by JoseTorrez on 8/25/2017.
  */
-public class FizzBuzz {
+public final class FizzBuzz {
+  /**
+   * Default Constructor.
+   */
+  private FizzBuzz() {
+
+  }
+  /**
+   * @param number Integer received.
+   * @return String word or number.
+   */
+  public static String numberDivisor(Integer number) {
+    String variable = "";
+    while (number > 0) {
+      if ((number % 3 == 0) && (number % 5 == 0)) {
+        return "FizzBuzz";
+      }
+      if (number % 3 == 0) {
+        return "Fizz";
+      }
+      if (number % 5 == 0) {
+        return "Buzz";
+      } else {
+        return Integer.toString(number);
+      }
+    }
+    return variable;
+  }
 }
--- src/test/java/org/fundacionjala/coding/jose/FizzBuzzTest.java
+++ src/test/java/org/fundacionjala/coding/jose/FizzBuzzTest.java
@@ -1,7 +1,32 @@
 package org.fundacionjala.coding.jose;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 /**
  * Created by JoseTorrez on 8/25/2017.
  */
 public class FizzBuzzTest {
+
+  /**
+   * Basic Tests.
+   */
+  @Test
+  public void basicTest() {
+    assertEquals("1", FizzBuzz.numberDivisor(1));
+    assertEquals("Fizz", FizzBuzz.numberDivisor(3));
+    assertEquals("Buzz", FizzBuzz.numberDivisor(5));
+    assertEquals("13", FizzBuzz.numberDivisor(13));
+    assertEquals("Fizz", FizzBuzz.numberDivisor(12));
+    assertEquals("FizzBuzz", FizzBuzz.numberDivisor(15));
+    assertEquals("113", FizzBuzz.numberDivisor(113));
+    assertEquals("Fizz", FizzBuzz.numberDivisor(123));
+    assertEquals("Buzz", FizzBuzz.numberDivisor(125));
+    assertEquals("173", FizzBuzz.numberDivisor(173));
+    assertEquals("FizzBuzz", FizzBuzz.numberDivisor(900));
+    assertEquals("FizzBuzz", FizzBuzz.numberDivisor(150));
+
+
+  }
 }
