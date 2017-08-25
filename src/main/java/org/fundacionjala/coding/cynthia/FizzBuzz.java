90bc42ae21108dad2f583cd967de5b01f5b83fe9
CynthiaTerrazas <cynthia.terrazas@fundacion-jala.org>
2017-08-25 23:19:20 +0000

    solution for fizzBuzz

--- src/main/java/org/fundacionjala/coding/cynthia/FizzBuzz.java
+++ src/main/java/org/fundacionjala/coding/cynthia/FizzBuzz.java
@@ -1,7 +1,24 @@
 package org.fundacionjala.coding.cynthia;
 
 /**
- * Created by Administrator on 8/25/2017.
+ * Created by Cynthia on 8/25/2017.
  */
 public class FizzBuzz {
+    /**
+     * @param num value for check.
+     * @return res answer.
+     */
+    public String isFizz(int num) {
+        String res = "";
+        if (num % 3 == 0 || num % 5 == 0) {
+            res = (num % 3 == 0) ? "Fizz" : "Buzz";
+        } else {
+            res = Integer.toString(num);
+        }
+
+        if (num % 3 == 0 && num % 5 == 0) {
+            res = "FizzBuzz";
+        }
+        return res;
+    }
 }
--- src/test/java/org/fundacionjala/coding/cynthia/FizzBuzzTest.java
+++ src/test/java/org/fundacionjala/coding/cynthia/FizzBuzzTest.java
@@ -1,7 +1,33 @@
 package org.fundacionjala.coding.cynthia;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 /**
  * Created by Administrator on 8/25/2017.
  */
 public class FizzBuzzTest {
+    /**
+     * tests.
+     */
+    @Test
+    public void basicTests() {
+        FizzBuzz fb = new FizzBuzz();
+        assertEquals("1", fb.isFizz(1));
+        assertEquals("2", fb.isFizz(2));
+        assertEquals("Fizz", fb.isFizz(3));
+        assertEquals("4", fb.isFizz(4));
+        assertEquals("Buzz", fb.isFizz(5));
+        assertEquals("Fizz", fb.isFizz(6));
+        assertEquals("7", fb.isFizz(7));
+        assertEquals("8", fb.isFizz(8));
+        assertEquals("Fizz", fb.isFizz(9));
+        assertEquals("Buzz", fb.isFizz(10));
+        assertEquals("11", fb.isFizz(11));
+        assertEquals("Fizz", fb.isFizz(12));
+        assertEquals("FizzBuzz", fb.isFizz(15));
+
+
+    }
 }
