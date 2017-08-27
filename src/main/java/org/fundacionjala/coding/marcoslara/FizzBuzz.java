a9026292db2c35808359cf90f162d4a58634a361
Marcos Lara <marcos.x86@outlook.com>
2017-08-27 19:54:35 -0400

    FizzBuzz Kata Solution Implemented (#119)
    * FizzBuzz Kata Solution Implemented
    
    * Implementation improvements
    
    * Use of primitive constants improved

---
+++ src/main/java/org/fundacionjala/coding/marcoslara/FizzBuzz.java
@@ -0,0 +1,63 @@
+package org.fundacionjala.coding.marcoslara;
+
+/**
+ * Created by Marcos on 8/25/2017.
+ * This class return "Fizz" string of multiples of three
+ * and "Buzz" of multiples of five.
+ */
+public final class FizzBuzz {
+
+    private static final int FIVE = 5;
+    private static final int THREE = 3;
+    private static final int ZERO = 0;
+    protected static final String FIZZ = "Fizz";
+    protected static final String BUZZ = "Buzz";
+    protected static final String FIZZ_BUZZ = "FizzBuzz";
+
+    /**
+     * Private constructor for the utility class.
+     */
+    private FizzBuzz() {
+
+    }
+
+    /**
+     * This method return the representation of a number based on the rules
+     * described.
+     *
+     * @param n is the number to be evaluated.
+     * @return the number representation.
+     */
+    public static String printNumber(int n) {
+        return n % THREE == ZERO || n % FIVE == ZERO || hasThisNumber(n, THREE) || hasThisNumber(n, FIVE)
+                ? printWord(n) : String.valueOf(n);
+    }
+
+    /**
+     * This method return the representation of numbers multiples of three and five.
+     *
+     * @param n is the number to be evaluated.
+     * @return the string representation based in the multiples.
+     */
+    private static String printWord(int n) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (n % THREE == ZERO || hasThisNumber(n, THREE)) {
+            stringBuilder.append(FIZZ);
+        }
+        if (n % FIVE == ZERO || hasThisNumber(n, FIVE)) {
+            stringBuilder.append(BUZZ);
+        }
+        return stringBuilder.toString();
+    }
+
+    /**
+     * This method check if a number contains a certain digit.
+     *
+     * @param n     is the number to be evaluated.
+     * @param digit is the number to check.
+     * @return the find result.
+     */
+    private static boolean hasThisNumber(int n, int digit) {
+        return String.valueOf(n).contains(String.valueOf(digit));
+    }
+}
---
+++ src/test/java/org/fundacionjala/coding/marcoslara/FizzBuzzTest.java
@@ -0,0 +1,102 @@
+package org.fundacionjala.coding.marcoslara;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Marcos on 8/25/2017.
+ * Unit test for Fizz Buzz Class.
+ */
+public class FizzBuzzTest {
+
+    /**
+     * This test assert the modifier of the Sequence private constructor class.
+     *
+     * @throws NoSuchMethodException     throw when no private constructor is defined.
+     * @throws IllegalAccessException    throw when can not access to the constructor.
+     * @throws InvocationTargetException throw when can not be perform a invocation.
+     * @throws InstantiationException    throw when a instantiation can not be performed.
+     */
+    @Test
+    public void privateConstructorTest() throws NoSuchMethodException, IllegalAccessException,
+            InvocationTargetException, InstantiationException {
+        Constructor<FizzBuzz> constructor = FizzBuzz.class.getDeclaredConstructor();
+        assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+        constructor.setAccessible(true);
+        constructor.newInstance();
+    }
+
+
+    /**
+     * Basic test for the number one.
+     */
+    @Test
+    public void fizzBuzzOfOne() {
+        assertEquals("1", FizzBuzz.printNumber(1));
+    }
+
+    /**
+     * Basic test for the number three.
+     */
+    @Test
+    public void fizzBuzzOfThree() {
+        assertEquals(FizzBuzz.FIZZ, FizzBuzz.printNumber(3));
+    }
+
+    /**
+     * Basic tets for the number five.
+     */
+    @Test
+    public void fizzBuzzOfFive() {
+        assertEquals(FizzBuzz.BUZZ, FizzBuzz.printNumber(5));
+    }
+
+    /**
+     * Basic tets for the number eleven.
+     */
+    @Test
+    public void fizzBuzzOfEleven() {
+        assertEquals("11", FizzBuzz.printNumber(11));
+    }
+
+    /**
+     * Basic test for the number fifteen.
+     */
+    @Test
+    public void fizzBuzzOfFifteen() {
+        assertEquals(FizzBuzz.FIZZ_BUZZ, FizzBuzz.printNumber(15));
+    }
+
+    /**
+     * For the stage two.
+     * Basic test for the number fifty three.
+     */
+    @Test
+    public void fizzBuzzO355fFiftyThree() {
+        assertEquals(FizzBuzz.FIZZ_BUZZ, FizzBuzz.printNumber(53));
+    }
+
+    /**
+     * For the stage two.
+     * Basic test for the number Three thousand five hundred fifty three.
+     */
+    @Test
+    public void fizzBuzzOfThreeThousandFiveHundredFiftyThree() {
+        assertEquals(FizzBuzz.FIZZ_BUZZ, FizzBuzz.printNumber(3553));
+    }
+
+    /**
+     * For the stage two.
+     * Basic test for the number Three million thirty thousand five hundred three.
+     */
+    @Test
+    public void fizzBuzzOfThreeMillionThirtyThousandFiveHundredThree() {
+        assertEquals(FizzBuzz.FIZZ_BUZZ, FizzBuzz.printNumber(3030503));
+    }
+}
