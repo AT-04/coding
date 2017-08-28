6b4b297c97232d05dec79216e19e21056c6c0e68
RichardSeverich <richard.severich@fundacion-jala.org>
2017-08-28 12:47:07 -0400

    Richard_Sequence (#91)
    * Sequence
    
    * Fixed Coverage
    
    * Fixed Sequence

---
+++ src/main/java/org/fundacionjala/coding/richard/Sequence.java
@@ -0,0 +1,20 @@
+package org.fundacionjala.coding.richard;
+
+/**
+ * Created by RichardSeverich on 8/18/2017.
+ */
+public class Sequence {
+
+    /**
+     * @param num is the number to convert.
+     * @return the sequence number.
+     */
+    long getScore(long num) {
+        long result = 0;
+        while (num > 0) {
+            result += num;
+            num -= 1;
+        }
+        return result * 50;
+    }
+}
