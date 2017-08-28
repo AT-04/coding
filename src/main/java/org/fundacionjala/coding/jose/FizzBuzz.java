56f5ae5e344003a040320458ee2652e86ccac22f
Jose Torrez <Jose.Torrez@fundacion-jala.org>
2017-08-28 16:45:25 -0400

    Adding suggestions

--- src/main/java/org/fundacionjala/coding/jose/FizzBuzz.java
+++ src/main/java/org/fundacionjala/coding/jose/FizzBuzz.java
@@ -4,6 +4,14 @@ package org.fundacionjala.coding.jose;
  * Created by JoseTorrez on 8/25/2017.
  */
 public final class FizzBuzz {
+
+  public static final String FIZZ_BUZZ = "FizzBuzz";
+  public static final int THREE = 3;
+  public static final int FIVE = 5;
+  public static final int ZERO = 0;
+  public static final String FIZZ = "Fizz";
+  public static final String BUZZ = "Buzz";
+
   /**
    * Default Constructor.
    */
@@ -16,21 +24,20 @@ public final class FizzBuzz {
    * @return String word or number.
    */
   public static String numberDivisor(Integer number) {
-    String variable = "";
     while (number > 0) {
-      if (((number % 3 == 0) && (number % 5 == 0)) || (Integer.toString(number).contains("3")
-          && Integer.toString(number).contains("5"))) {
-        return "FizzBuzz";
+      if ((number % THREE == ZERO) && (number % FIVE == ZERO)
+          || Integer.toString(number).contains("3") && Integer.toString(number).contains("5")) {
+        return FIZZ_BUZZ;
       }
-      if ((number % 3 == 0) || (Integer.toString(number).contains("3"))) {
-        return "Fizz";
+      if ((number % THREE == ZERO) || (Integer.toString(number).contains("3"))) {
+        return FIZZ;
       }
-      if ((number % 5 == 0) || (Integer.toString(number).contains("5"))) {
-        return "Buzz";
+      if ((number % FIVE == ZERO) || (Integer.toString(number).contains("5"))) {
+        return BUZZ;
       } else {
         return Integer.toString(number);
       }
     }
-    return variable;
+    return "";
   }
 }
