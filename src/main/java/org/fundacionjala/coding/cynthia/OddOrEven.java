0af5c1445d8340d9e28e4a75df7e954b6596960f
CynthiaTerrazas <cynthia.terrazas@fundacion-jala.org>
2017-08-29 00:10:28 +0000

    the solution for odd or even kata.

--- src/main/java/org/fundacionjala/coding/cynthia/OddOrEven.java
+++ src/main/java/org/fundacionjala/coding/cynthia/OddOrEven.java
@@ -2,44 +2,27 @@ package org.fundacionjala.coding.cynthia;
 
 import static java.util.Arrays.stream;
 
+
 /**
- * Given an array of numbers, determine whether the sum of all of the numbers is odd or even.
-
- Give your answer in string format as 'odd' or 'even'.
-
- If the input array is empty consider it as: [0] (array with a zero).
-
- Example:
-
- oddOrEven([0]) returns "even"
- oddOrEven([2, 5, 34, 6]) returns "odd"
- oddOrEven([0, -1, -5]) returns "even"
-
- Have fun!
- .
+ * Given an array of numbers, determine whether the sum
+ * of all of the numbers is odd or even.
+ * Give your answer in string format as 'odd' or 'even'
+ * If the input array is empty consider it as: [0] (array with a zero).
+ * Example:
+ * determineOddOrEven([0]) returns "even"
+ * determineOddOrEven([2, 5, 34, 6]) returns "odd"
+ * determineOddOrEven([0, -1, -5]) returns "even"
  */
-public class DetermineOddOrEven {
-    public static String oddOrEven(int[] array) {
+public class OddOrEven {
+
+    /**
+     * the method that determine if is odd or even.
+     *
+     * @param array with the numbers to sum.
+     * @return the respective value (even or odd).
+     */
+    public String determineOddOrEven(int[] array) {
         return stream(array).sum() % 2 == 0 ? "even" : "odd";
     }
 }
 
-/**
- * import static java.util.Arrays.stream;
-
- class Codewars {
- static String oddOrEven(final int[] array) {
- return stream(array).sum() % 2 == 0 ? "even" : "odd";
- }
- }
-
- ====================================
-
- import java.util.stream.*;
-
- public class Codewars {
- public static String oddOrEven(int[] array) {
- return (IntStream.of(array).sum() & 1) == 1 ? "odd" : "even";
- }
- }
- **/
--- src/test/java/org/fundacionjala/coding/cynthia/OddOrEvenTest.java
+++ src/test/java/org/fundacionjala/coding/cynthia/OddOrEvenTest.java
@@ -1,21 +1,25 @@
-package test.java.org.fundacionjala.coding.cynthia;
+package org.fundacionjala.coding.cynthia;
 
 import static org.junit.Assert.assertEquals;
 
-import main.java.org.fundacionjala.coding.cynthia.DetermineOddOrEven;
 import org.junit.Test;
 
 /**
  * Created by Administrator on 8/23/2017.
  */
-public class DetermineOddOrEvenTest {
+public class OddOrEvenTest {
+
+    /**
+     * Tests.
+     */
     @Test
     public void exampleTest() {
-        assertEquals("odd", DetermineOddOrEven.oddOrEven(new int[] {2, 5, 34, 6}));
-        assertEquals("even", DetermineOddOrEven.oddOrEven(new int[] {0}));
-        assertEquals("even", DetermineOddOrEven.oddOrEven(new int[] {0, -1, -5}));
-        assertEquals("odd", DetermineOddOrEven.oddOrEven(new int[] {10, 10, 10, 10 , 10, 10, 1}));
-        assertEquals("even", DetermineOddOrEven.oddOrEven(new int[] {1, 1, 1, 1 , 1, 1}));
-        assertEquals("odd", DetermineOddOrEven.oddOrEven(new int[] {25}));
+        OddOrEven oddEven = new OddOrEven();
+        assertEquals("odd", oddEven.determineOddOrEven(new int[] {2, 5, 34, 6}));
+        assertEquals("even", oddEven.determineOddOrEven(new int[] {0}));
+        assertEquals("even", oddEven.determineOddOrEven(new int[] {0, -1, -5}));
+        assertEquals("odd", oddEven.determineOddOrEven(new int[] {10, 10, 10, 10, 10, 10, 1}));
+        assertEquals("even", oddEven.determineOddOrEven(new int[] {1, 1, 1, 1, 1, 1}));
+        assertEquals("odd", oddEven.determineOddOrEven(new int[] {25}));
     }
 }
