9defc0175c081ee99f3dfd03a20a61271689e771
CynthiaTerrazas <cynthia.terrazas@fundacion-jala.org>
2017-09-05 23:00:12 +0000

    This is my solution for Most frequent

---
+++ src/main/java/org/fundacionjala/coding/cynthia/MostFrequent.java
@@ -0,0 +1,7 @@
+package org.fundacionjala.coding.cynthia;
+
+/**
+ * Created by Administrator on 9/5/2017.
+ */
+public class MostFrequent {
+}
---
+++ src/test/java/org/fundacionjala/coding/cynthia/MostFrequentTest.java
@@ -0,0 +1,8 @@
+package org.fundacionjala.coding;
+
+/**
+ * Created by Administrator on 9/5/2017.
+ */
+public class MostFrequentTest {
+
+}
