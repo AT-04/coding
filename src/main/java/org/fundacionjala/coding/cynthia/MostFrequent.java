43e8f38992d379ea260ca563509a32bc9d202605
CynthiaTerrazas <cynthia.terrazas@fundacion-jala.org>
2017-09-05 23:00:23 +0000

    This is my solution for Most frequent

--- src/main/java/org/fundacionjala/coding/cynthia/MostFrequent.java
+++ src/main/java/org/fundacionjala/coding/cynthia/MostFrequent.java
@@ -1,7 +1,27 @@
 package org.fundacionjala.coding.cynthia;
 
 /**
- * Created by Administrator on 9/5/2017.
+ * Created by Cynthia on 9/5/2017.
  */
 public class MostFrequent {
+
+    /**
+     * @param a is the array.
+     * @return the most repeat.
+     */
+    public int mostFrequentItemCount(int[] a) {
+        int mayor = 0;
+        for (int anA : a) {
+            int count = 0;
+            for (int anA1 : a) {
+                if (anA1 == anA) {
+                    count++;
+                }
+            }
+            if (count > mayor) {
+                mayor = count;
+            }
+        }
+        return mayor;
+    }
 }
