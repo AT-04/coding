227c0b0ddfede98d719d38851dd77d7ab5c94860
JoseTorrez <Jose.Torrez@fundacion-jala.org>
2017-09-05 01:02:03 +0000

    Josetorrez fizzbuzz (#115)
    * Solution for Sequence of Numbers
    
    * FizzBuzz solution try
    
    * FizzBuzz solution try
    
    * adding stage 2
    
    * fixing Test Coverage
    
    * Adding suggestions
    
    * Removing while condition and useless parentheses.
    
    * Formatting lines.

---
+++ src/main/java/org/fundacionjala/coding/jose/FizzBuzz.java
@@ -0,0 +1,44 @@
+package org.fundacionjala.coding.jose;
+
+/**
+ * Created by JoseTorrez on 8/25/2017.
+ */
+public final class FizzBuzz {
+
+    public static final String FIZZ_BUZZ = "FizzBuzz";
+    public static final int THREE = 3;
+    public static final int FIVE = 5;
+    public static final int ZERO = 0;
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+    public static final String EMPTY = "";
+
+    /**
+     * Default Constructor.
+     */
+    private FizzBuzz() {
+
+    }
+
+    /**
+     * @param number Integer received.
+     * @return String word or number.
+     */
+    public static String numberDivisor(Integer number) {
+        if (number < 0) {
+            return EMPTY;
+        }
+        if (number % THREE == ZERO && number % FIVE == ZERO
+                || Integer.toString(number).contains("3") && Integer.toString(number).contains("5")) {
+            return FIZZ_BUZZ;
+        }
+        if (number % THREE == ZERO || Integer.toString(number).contains("3")) {
+            return FIZZ;
+        }
+        if (number % FIVE == ZERO || Integer.toString(number).contains("5")) {
+            return BUZZ;
+        } else {
+            return Integer.toString(number);
+        }
+    }
+}
---
+++ src/test/java/org/fundacionjala/coding/jose/FizzBuzzTest.java
@@ -0,0 +1,56 @@
+package org.fundacionjala.coding.jose;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Created by JoseTorrez on 8/25/2017.
+ */
+public class FizzBuzzTest {
+
+  /**
+   * This test assert the modifier of the Sequence private constructor class.
+   *
+   * @throws NoSuchMethodException     throw when no private constructor is defined.
+   * @throws IllegalAccessException    throw when can not access to the constructor.
+   * @throws InvocationTargetException throw when can not be perform a invocation.
+   * @throws InstantiationException    throw when a instantiation can not be performed.
+   */
+  @Test
+  public void privateConstructorTest() throws NoSuchMethodException, IllegalAccessException,
+      InvocationTargetException, InstantiationException {
+    Constructor<FizzBuzz> constructor = FizzBuzz.class.getDeclaredConstructor();
+    assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+    constructor.setAccessible(true);
+    constructor.newInstance();
+  }
+
+  /**
+   * Basic Tests.
+   */
+  @Test
+  public void basicTest() {
+    assertEquals("", FizzBuzz.numberDivisor(-1));
+    assertEquals("1", FizzBuzz.numberDivisor(1));
+    assertEquals("Fizz", FizzBuzz.numberDivisor(3));
+    assertEquals("Buzz", FizzBuzz.numberDivisor(5));
+    assertEquals("Fizz", FizzBuzz.numberDivisor(13));
+    assertEquals("Fizz", FizzBuzz.numberDivisor(12));
+    assertEquals("FizzBuzz", FizzBuzz.numberDivisor(15));
+    assertEquals("Fizz", FizzBuzz.numberDivisor(113));
+    assertEquals("Fizz", FizzBuzz.numberDivisor(123));
+    assertEquals("Buzz", FizzBuzz.numberDivisor(125));
+    assertEquals("Fizz", FizzBuzz.numberDivisor(173));
+    assertEquals("FizzBuzz", FizzBuzz.numberDivisor(900));
+    assertEquals("FizzBuzz", FizzBuzz.numberDivisor(150));
+    assertEquals("FizzBuzz", FizzBuzz.numberDivisor(53));
+
+
+  }
+}
