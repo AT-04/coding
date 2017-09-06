22b5f26a3686026964c6407ec51886316ccdfe9c
RichardSeverich <Richard.Severich@fundacion-jala.org>
2017-09-05 23:20:24 -0400

    Comments Add

--- src/test/java/org/fundacionjala/coding/richard/DivisibleByFiveTest.java
+++ src/test/java/org/fundacionjala/coding/richard/DivisibleByFiveTest.java
@@ -6,6 +6,20 @@ import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Richard on 9/5/2017.
+ * <p>
+ * Define a regular expression which tests if a given string representing a binary number is divisible by 5.
+ * Examples:
+ * 5 divisible by 5
+ * DivisibleByFive.pattern().matcher('101').matches() == true
+ * 35 divisible by 5
+ * DivisibleByFive.pattern().matcher('10000111').matches() == true
+ * 666 not divisible by 5
+ * DivisibleByFive.pattern().matcher('0000001010011010').matches() == false
+ * Note:
+ * This can be solved by creating a Finite State Machine that evaluates if a string representing a
+ * number in binary base is divisible by given number.
+ * The detailed explanation for dividing by 3 is here
+ * The FSM diagram for dividing by 5 is here
  */
 public class DivisibleByFiveTest {
 
