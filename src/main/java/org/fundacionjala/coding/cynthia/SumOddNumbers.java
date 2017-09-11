59f037443951723e0163fe68549722591dc97fde
CynthiaTerrazas <cynthia.terrazas@fundacion-jala.org>
2017-09-11 19:46:55 +0000

    this is my solution for this kata

--- src/main/java/org/fundacionjala/coding/cynthia/SumOddNumbers.java
+++ src/main/java/org/fundacionjala/coding/cynthia/SumOddNumbers.java
@@ -1,7 +1,29 @@
 package org.fundacionjala.coding.cynthia;
 
 /**
- * Created by Administrator on 9/11/2017.
+ * Given the triangle of consecutive odd numbers:
+
+ 1
+ 3     5
+ 7     9    11
+ 13    15    17    19
+ 21    23    25    27    29
+ ...
+
+ Calculate the row sums of this triangle from the row index (starting at index 1) e.g.:
+
+ row_sum_odd_numbers(1); # 1
+ row_sum_odd_numbers(2); # 3 + 5 = 8
+
  */
 public class SumOddNumbers {
+
+    /**
+     *
+     * @param n integer.
+     * @return the sum.
+     */
+    public int rowSumOddNumbers(int n) {
+        return ((int) Math.pow(n, 2)) * n;
+    }
 }
--- src/test/java/org/fundacionjala/coding/cynthia/SumOddNumbersTest.java
+++ src/test/java/org/fundacionjala/coding/cynthia/SumOddNumbersTest.java
@@ -1,7 +1,26 @@
 package org.fundacionjala.coding.cynthia;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 /**
- * Created by Administrator on 9/11/2017.
+ * Created by Cynthia on 9/11/2017.
  */
 public class SumOddNumbersTest {
+
+    /**
+     *
+     */
+        @Test
+        public void test1() {
+            SumOddNumbers sumOddNumbers = new SumOddNumbers();
+            assertEquals(1, sumOddNumbers.rowSumOddNumbers(1));
+            assertEquals(74088, sumOddNumbers.rowSumOddNumbers(42));
+            assertEquals(8, sumOddNumbers.rowSumOddNumbers(2));
+            assertEquals(27, sumOddNumbers.rowSumOddNumbers(3));
+            assertEquals(64, sumOddNumbers.rowSumOddNumbers(4));
+            assertEquals(125, sumOddNumbers.rowSumOddNumbers(5));
+        }
+
 }
