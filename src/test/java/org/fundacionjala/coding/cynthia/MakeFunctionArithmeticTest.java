24e9397b441f00c509c47ce0841fc410f49c4132
CynthiaTerrazas <cynthia.terrazas@fundacion-jala.org>
2017-09-11 20:49:58 +0000

    add more tests

--- src/test/java/org/fundacionjala/coding/cynthia/MakeFunctionArithmeticTest.java
+++ src/test/java/org/fundacionjala/coding/cynthia/MakeFunctionArithmeticTest.java
@@ -19,5 +19,10 @@ public class MakeFunctionArithmeticTest {
         assertEquals("'subtract' should return a mimus b!", 6, make.arithmetic(8, 2, "subtract"));
         assertEquals("'multiply' should return a multiplied by b!", 10, make.arithmetic(5, 2, "multiply"));
         assertEquals("'divide' should return a divided by b!", 4, make.arithmetic(8, 2, "divide"));
+        assertEquals("'' should return a divided by b!", 0, make.arithmetic(8, 2, " "));
+        assertEquals("'add' should return the two numbers added together!", 200, make.arithmetic(100, 100, "add"));
+        assertEquals("'subtract' should return a mimus b!", 0, make.arithmetic(1200, 1200, "subtract"));
+        assertEquals("'multiply' should return a multiplied by b!", 0, make.arithmetic(53454657, 0, "multiply"));
+        assertEquals("'divide' should return a divided by b!", 0, make.arithmetic(0, 345562, "divide"));
     }
 }
