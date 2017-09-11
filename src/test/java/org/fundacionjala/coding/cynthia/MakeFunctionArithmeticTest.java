a3a78773b47c28180f9450ab54631a8ffa821fac
CynthiaTerrazas <cynthia.terrazas@fundacion-jala.org>
2017-09-11 20:12:38 +0000

    this is my solution for this kata

--- src/test/java/org/fundacionjala/coding/cynthia/MakeFunctionArithmeticTest.java
+++ src/test/java/org/fundacionjala/coding/cynthia/MakeFunctionArithmeticTest.java
@@ -1,7 +1,23 @@
 package org.fundacionjala.coding.cynthia;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 /**
- * Created by Administrator on 9/11/2017.
+ * Created by Cynthia on 9/11/2017.
  */
 public class MakeFunctionArithmeticTest {
+
+    /**
+     *
+     */
+    @Test
+    public void testBasic() {
+        MakeFunctionArithmetic make = new MakeFunctionArithmetic();
+        assertEquals("'add' should return the two numbers added together!", 3, make.arithmetic(1, 2, "add"));
+        assertEquals("'subtract' should return a mimus b!", 6, make.arithmetic(8, 2, "subtract"));
+        assertEquals("'multiply' should return a multiplied by b!", 10, make.arithmetic(5, 2, "multiply"));
+        assertEquals("'divide' should return a divided by b!", 4, make.arithmetic(8, 2, "divide"));
+    }
 }
